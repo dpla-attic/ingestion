b2f8a240a3f13bbf272b43464702dd45386f603c
Mark Baker <mark@zepheira.com>
2012-10-14 00:52:50 -0400

    Minor select-id service fix

--- lib/akamod/select-id.py
+++ lib/akamod/select-id.py
@@ -3,7 +3,7 @@ from akara.services import simple_service
 from amara.thirdparty import json
 
 @simple_service('POST', 'http://purl.org/la/dp/select-id', 'select-id', 'application/json')
-def select_id(body,ctype,prop=None):
+def selid(body,ctype,prop=None):
     '''   
     Service that accepts a JSON document and adds or sets the "id" property to the
     value of the property named by the "prop" paramater
