4a6641e5e34e8f43be789b85186bae68f0f204a2
Mark Baker <mark@zepheira.com>
2012-10-17 22:23:17 -0400

    Add test case framework from Akara, plus some enrichment tests

---
+++ test/test_enrich.py
@@ -0,0 +1,70 @@
+from server_support import server
+
+import httplib2
+import os
+from amara.thirdparty import json
+
+CT_JSON = {"Content-Type": "application/json"}
+
+H = httplib2.Http()
+
+def test_shred1():
+    "Valid shredding"
+
+    INPUT = {
+        "id": "999",
+        "prop1": "lets,go,bluejays"
+    }
+    EXPECTED = {
+        "id": "999",
+        "prop1": ["lets","go","bluejays"]
+    }
+    url = server() + "shred?prop=prop1"
+    resp,content = H.request(url,"POST",body=json.dumps(INPUT),headers=CT_JSON)
+    assert str(resp.status).startswith("2")
+
+    assert json.loads(content) == EXPECTED
+
+def test_shred2():
+    "Shredding of an unknown property"
+    INPUT = {
+        "id": "999",
+        "prop1": "lets,go,bluejays"
+    }
+    EXPECTED = INPUT
+    url = server() + "shred?prop=prop9"
+    resp,content = H.request(url,"POST",body=json.dumps(INPUT),headers=CT_JSON)
+    assert str(resp.status).startswith("2")
+
+    assert json.loads(content) == EXPECTED
+
+def test_unshred1():
+    "Valid unshredding"
+
+    INPUT = {
+        "id": "999",
+        "prop1": ["lets","go","bluejays"]
+    }
+    EXPECTED = {
+        "id": "999",
+        "prop1": "lets,go,bluejays"
+    }
+    url = server() + "unshred?prop=prop1"
+    resp,content = H.request(url,"POST",body=json.dumps(INPUT),headers=CT_JSON)
+
+    assert json.loads(content) == EXPECTED
+
+def test_unshred2():
+    "Unshredding of an unknown property"
+    INPUT = {
+        "id": "999",
+        "prop1": ["lets","go","bluejays"]
+    }
+    EXPECTED = INPUT
+    url = server() + "unshred?prop=prop9"
+    resp,content = H.request(url,"POST",body=json.dumps(INPUT),headers=CT_JSON)
+
+    assert json.loads(content) == EXPECTED
+
+if __name__ == "__main__":
+    raise SystemExit("Use nosetests")
