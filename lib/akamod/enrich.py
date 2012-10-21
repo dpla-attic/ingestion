e4493b4b62c7e1bc9270aa70e76d576113d9a8ce
Mark Baker <mark@zepheira.com>
2012-10-21 19:38:02 -0400

    Implement split-pipelining for collections and records, connect storage to fetching for now

--- lib/akamod/enrich.py
+++ lib/akamod/enrich.py
@@ -1,30 +1,52 @@
 from akara.services import simple_service
 from akara import request, response
-from akara import module_config
+from akara import module_config, logger
 from akara.util import copy_headers_to_dict
+from amara.thirdparty import json
+import uuid
 
 import httplib2
+
+H = httplib2.Http()
+H.force_exception_as_status_code = True
+
+def pipe(content,ctype,enrichments,wsgi_header):
+    body = json.dumps(content)
+    for uri in enrichments:
+        headers = copy_headers_to_dict(request.environ,exclude=[wsgi_header])
+        headers['content-type'] = ctype
+        resp, cont = H.request(uri,'POST',body=body,headers=headers)
+        if not str(resp.status).startswith('2'):
+            logger.debug("Error in enrichment pipeline at %s: %s"%(uri,repr(resp)))
+            continue
+
+        body = cont
 
 @simple_service('POST', 'http://purl.org/la/dp/enrich', 'enrich', 'application/json')
 def enrich(body,ctype):
     '''   
     Establishes a pipeline of services identified by an ordered list of URIs provided
-    in the Pipeline request header to a PUT request
+    in two request headers, one for collections and one for records
     '''   
     
-    enrichments = request.environ.get(u'HTTP_PIPELINE','').split(',')
+    coll_enrichments = request.environ.get(u'HTTP_PIPELINE_COLL','').split(',')
+    rec_enrichments = request.environ.get(u'HTTP_PIPELINE_REC','').split(',')
 
-    H = httplib2.Http()
-    H.force_exception_as_status_code = True
+    data = json.loads(body)
 
-    content = body
-    for uri in enrichments:
-        headers = copy_headers_to_dict(request.environ,exclude=['HTTP_PIPELINE'])
-        headers['content-type'] = ctype
-        resp, content = H.request(uri,'POST',body=content,headers=headers)
-        if not str(resp.status).startswith('2'):
-            response.code = resp.status
-            response.add_header('content-type',resp['content-type'])
-            break
+    # First, we run the collection representation through its enrichment pipeline
 
-    return content
+    COLL = {
+        "id": str(uuid.uuid4()),
+    }
+
+    pipe(COLL, ctype, coll_enrichments, 'HTTP_PIPELINE_COLL')
+
+    # Then the records
+    for record in data[u'items']:
+        # Assign the record to its collection
+        record['collection'] = COLL['id']
+
+        pipe(record, ctype, rec_enrichments, 'HTTP_PIPELINE_REC')
+    
+    return json.dumps({})
