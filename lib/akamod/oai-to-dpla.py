5e778611c1a8543f6a6919845e6e4d047a438fcd
Mark Baker <mark@zepheira.com>
2012-10-28 22:06:49 -0400

    Support specification of lat/long property in profile using geoprop query param

--- lib/akamod/oai-to-dpla.py
+++ lib/akamod/oai-to-dpla.py
@@ -4,6 +4,8 @@ from akara.services import simple_service
 from amara.thirdparty import json
 from functools import partial
 
+GEOPROP = None
+
 CONTEXT = {
    "@vocab": "http://purl.org/dc/terms/",
    "dpla": "http://dp.la/terms/",
@@ -26,12 +28,13 @@ CONTEXT = {
 }
 
 def spatial_transform(d):
+    global GEOPROP
     spatial = []
     for i,s in enumerate(d["coverage"]):
         sp = { "name": s }
         # Check if we have lat/long for this location. Requires geocode earlier in the pipeline
-        if "coverage_geo" in d and i < len(d["coverage_geo"]) and len(d["coverage_geo"][i]) > 0:
-            sp["coordinates"] = d["coverage_geo"][i]
+        if GEOPROP in d and i < len(d[GEOPROP]) and len(d[GEOPROP][i]) > 0:
+            sp["coordinates"] = d[GEOPROP][i]
         spatial.append(sp);
     return {"spatial":spatial}
 
@@ -74,14 +77,15 @@ TRANSFORMER = {
 }
 
 @simple_service('POST', 'http://purl.org/la/dp/oai-to-dpla', 'oai-to-dpla', 'application/ld+json')
-def oaitodpla(body,ctype,dplacontrib=None):
+def oaitodpla(body,ctype,dplacontrib=None,geoprop=None):
     '''   
     Convert output of Freemix OAI service into the DPLA JSON-LD format.
 
     Does not currently require any enrichments to be ahead in the pipeline, but
     supports geocoding if used. In the future, subject shredding may be assumed too.
 
-    Accepts a single parameter which specifies the literal value of the dplaContributor stanza
+    Parameter "dplacontrib" specifies the literal value of the dplaContributor stanza
+    Parameter "geoprop" specifies the property name containing lat/long coords
     '''
 
     try :
@@ -90,6 +94,9 @@ def oaitodpla(body,ctype,dplacontrib=None):
         response.code = 500
         response.add_header('content-type','text/plain')
         return "Unable to parse body as JSON"
+
+    global GEOPROP
+    GEOPROP = geoprop
 
     out = {
         "@context": CONTEXT,
