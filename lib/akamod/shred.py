d28d9b1a74a6781840de662457499b52666dc384
Mark Baker <mark@zepheira.com>
2012-11-22 12:24:39 -0500

    document multiprop shredding

--- lib/akamod/shred.py
+++ lib/akamod/shred.py
@@ -6,8 +6,12 @@ from amara.thirdparty import json
 @simple_service('POST', 'http://purl.org/la/dp/shred', 'shred', 'application/json')
 def shred(body,ctype,action="shred",prop=None,delim=','):
     '''   
-    Service that accepts a JSON document and "unshreds" the value of the
-    field named by the "prop" parameter
+    Service that accepts a JSON document and "shreds" or "unshreds" the value
+    of the field(s) named by the "prop" parameter
+
+    "prop" can include multiple property names, delimited by a comma (the delim
+    property is used only for the fields to be shredded/unshredded). This requires
+    that the fields share a common delimiter however.
     '''   
     
     try :
