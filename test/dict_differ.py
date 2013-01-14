b5a2ac71f5d4a10ad5c970281742f93e8ff53f2f
Szymon Guz <szymon@endpoint.com>
2013-01-14 13:56:01 -0500

    Changes in DictDiffer.

--- test/dict_differ.py
+++ test/dict_differ.py
@@ -1,16 +1,53 @@
-class DictDiffer():
 
-    def diff(self, first, second):
+
+class DictDiffer:
+    """
+    Class for creating nicely looking dictionary diffs.
+    """
+
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+        self._diff = self._generate_diff()
+        print self._diff
+
+    def _generate_diff(self):
+        """
+        Returns:
+            Dictionary difference as a dictionary.
+        """
         diff = {}
-        for k in first.keys():
-            if not second.has_key(k):
-                diff[k] = (first[k], "KEY NOT FOUND")
-            elif first[k] != second[k]:
-                diff[k] = (first[k], second[k])
-        for k in second.keys():         
-            if not first.has_key(k):
-                diff[k] = (second[k], "KEY NOT FOUND")                
+        for k in self.first.keys():
+            if not self.second.has_key(k):
+                diff[k] = (self.first[k], "KEY NOT FOUND")
+            elif self.first[k] != self.second[k]:
+                diff[k] = (self.first[k], self.second[k])
+        for k in self.second.keys():         
+            if not self.first.has_key(k):
+                diff[k] = (self.second[k], "KEY NOT FOUND")                
         return diff
             
-    def same(self, first, second):
-        return self.diff(first, second) == {}
+    def same(self):
+        """
+        Returns:
+            True if provided dictionaries are the same.
+            False otherwise.
+        """
+        return self._diff == {}
+
+    def diff(self):
+        """
+        Returns:
+            Dictionary difference between the dictionaries.
+        """
+        return self._diff
+
+    def print_diff(self):
+        """
+        Returns:
+            Prints nicely the difference between dictionaries.
+        """
+        import pprint
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint( self._diff )
+        print "aaaa"
