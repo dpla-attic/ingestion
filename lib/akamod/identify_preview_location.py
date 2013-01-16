d2898146644df0b7837758f9ab38652bbd31bf58
Szymon Guz <szymon@endpoint.com>
2013-01-16 12:21:45 -0500

    Some refactoring:
    * Fixed a typo.
    * 500 is returned only if the JSON is not parseable.
    * All other errors are logged and then the input is returned.

--- lib/akamod/identify_preview_location.py
+++ lib/akamod/identify_preview_location.py
@@ -3,35 +3,48 @@ from akara import response
 from akara.services import simple_service
 from amara.thirdparty import json
 
-@simple_service('POST', 'http://purl.org/la/dp/indentify_preview_location', 'identify_preview_location', 'application/json')
+@simple_service('POST', 'http://purl.org/la/dp/identify_preview_location', 'identify_preview_location', 'application/json')
 def identify_preview_location(body, ctype):
     """
     Responsible for: adding a field to a document with the URL where we should 
     expect to the find the thumbnail
     """
     
+    data = {}
     try:
         data = json.loads(body)
-        url = data['source']
-        logger.debug("source = " + source)
-        URL_FIELD_NAME = u"preview_source_url"
-        (base_url, rest) = url.split("u?")
-        if base_url == "" or rest == "":
-            raise Exception("Bad URL.")
-
-        p = rest.split(",")
-        if len(p) != 2:
-            raise Exception("Bad URL.")
-
-        thumb_url = "%scgi-bin/thumbnail.exe?CISOROOT=%s&amp;CISOPTR=%s" % (base_url, p[0], p[1])
-        data[URL_FIELD_NAME] = thumb_url
-        logger.debug("Thumbnail URL = " + thumb_url)
-        return json.dumps(data)
-        
     except Exception as e:
         response.code = 500
         response.add_header('content-type', 'text/plain')
         return e.message
+
+    url = data['source']
+    logger.debug("source = " + url)
+    URL_FIELD_NAME = u"preview_source_url"
+    p = url.split("u?")
+
+    if len(p) != 2:
+        logger.error("Bad URL %s. It should have just one 'u?' part." % url)
+        return body
+
+    (base_url, rest) = p
+
+    if base_url == "" or rest == "":
+        logger.error("Bad URL: %s. There is no 'u?' part." % url)
+        return body
+
+    p = rest.split(",")
+
+    if len(p) != 2:
+        logger.error("Bad URL %s. Expected two parts at the end, used in thumbnail URL for CISOROOT and CISOPTR." %url)
+        return body
+
+    thumb_url = "%scgi-bin/thumbnail.exe?CISOROOT=%s&amp;CISOPTR=%s" % (base_url, p[0], p[1])
+    data[URL_FIELD_NAME] = thumb_url
+
+    logger.debug("Thumbnail URL = " + thumb_url)
+    return json.dumps(data)
+        
 
 
 
