774292cc66d7b89e28ce740523786632ad6a932c
Szymon Guz <szymon@endpoint.com>
2013-01-16 15:04:01 -0500

    Fixed a bug in identify_preview_location with missing 'source' field.

--- lib/akamod/identify_preview_location.py
+++ lib/akamod/identify_preview_location.py
@@ -18,6 +18,10 @@ def identify_preview_location(body, ctype):
         response.add_header('content-type', 'text/plain')
         return e.message
 
+    if not data.has_key("source"):
+        logger.error("There is no 'source' key in JSON")
+        return body
+
     url = data['source']
     logger.debug("source = " + url)
     URL_FIELD_NAME = u"preview_source_url"
