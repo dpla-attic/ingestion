efe82d4ad4fa7e76c3a64cadbc1815a23e157146
Szymon Guz <szymon@endpoint.com>
2013-01-22 09:57:28 -0500

    Added tests for downloading images in jpg/png formats.

--- test/test_download_preview.py
+++ test/test_download_preview.py
@@ -76,7 +76,7 @@ def test_download_preview():
         path = get_thumbs_root()
         for i in xrange(0,32,2):
             path = os.path.join(path, md5[i:i+2])
-        path = os.path.join(path, id+".jpeg")
+        path = os.path.join(path, id+".jpg")
         return path
     
     INPUT = json.dumps(GOOD_DATA)
@@ -99,7 +99,47 @@ def test_downloading_with_bad_URL():
     assert resp.status == 200
     assert_same_jsons(INPUT, content)
 
+from dplaingestion.akamod.download_test_image import image_png, image_jpg
 
+def _check_downloading_image(extension, expected_content):
+    """
+    TODO: comment here
+    """
+    GOOD_DATA = { "id":"clemson--cfb004",
+            URL_FIELD_NAME: server() + 'download_test_image?extension=' + extension
+    }
+    INPUT = json.dumps(GOOD_DATA)
+    resp, content = H.request(url, "POST", body=INPUT, headers=HEADERS)
+    filepath = get_thumbs_root() + "/84/BA/8B/C3/2C/43/16/A9/6F/DC/89/F5/1B/EB/42/7D/clemson__cfb004." + extension
+    GOOD_DATA[URL_FILE_PATH] = filepath
+    OUTPUT = json.dumps(GOOD_DATA)
+    assert resp.status == 200
+    assert_same_jsons(OUTPUT, content)
+
+    # let's check if the files are the same
+    base64_downloaded_file = ""
+    with open(filepath, "rb") as f:
+        data = f.read()
+        print len(data)
+        base64_downloaded_file = data.encode("base64").rstrip('\n')
+    print base64_downloaded_file[-10:] + "|"
+    print expected_content[-10:] + "|"
+    print len(base64_downloaded_file)
+    print len(expected_content)
+    assert base64_downloaded_file == expected_content
+
+
+def test_dowloading_png_image():
+    """
+    Should return JSON with file path and download the file.
+    """
+    _check_downloading_image("png", image_png)
+
+def test_dowloading_jpg_image():
+    """
+    Should return JSON with file path and download the file.
+    """
+    _check_downloading_image("jpg", image_jpg)
 
 if __name__ == "__main__":
     raise SystemExit("Use nosetest")
