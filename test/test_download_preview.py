7f9c3b1cf746a3028e15742a4b7d24dd3fc85985
Szymon Guz <szymon@endpoint.com>
2013-01-22 10:29:00 -0500

    Test refactoring.

--- test/test_download_preview.py
+++ test/test_download_preview.py
@@ -1,8 +1,8 @@
+import os
 import sys
 from server_support import server, print_error_log, get_thumbs_root
-
+from dplaingestion.akamod.download_test_image import image_png, image_jpg
 from amara.thirdparty import httplib2
-import os
 from amara.thirdparty import json
 from dict_differ import DictDiffer, assert_same_jsons, pinfo
 
@@ -20,6 +20,7 @@ URL_FIELD_NAME = u"preview_source_url"
 # Used for storing the path to the local filename.
 URL_FILE_PATH = u"preview_file_path"
 
+# URL for current akara server instance.
 url = server() + "download_preview"
 
 def test_download_preview_bad_json():
@@ -51,6 +52,7 @@ def test_download_preview_without_thumbnail_url_field():
     assert resp.status == 200
     assert_same_jsons(INPUT, content)
 
+
 def test_download_preview_with_bad_url():
     """
     Should get 200 from akara and input JSON when there is bad thumbnail URL.
@@ -60,6 +62,31 @@ def test_download_preview_with_bad_url():
     assert resp.status == 200
     assert_same_jsons(INPUT, content)
 
+
+def _get_file_path(doc_id, extension):
+    """
+    Calculates the absolute filepath where the for 
+    given document id and file extension shoul be saved 
+    by the download_preview module.
+
+    Arguments:
+        doc_id      -   document id from couchdb
+        extension   -   file extension, without '.'
+
+    Returns:
+        Absolute path for the file.
+    """
+    import re
+    import hashlib
+    id = re.sub(r'[-]', '_', doc_id)
+    md5 = hashlib.md5(doc_id).hexdigest().upper()
+    md5_path = ""
+    for i in xrange(0,32,2):
+        md5_path = os.path.join(md5_path, md5[i:i+2])
+    path = os.path.join(get_thumbs_root(), md5_path, id + "." + extension)
+    return path
+
+
 def test_download_preview():
     """
     Should get 200 from akara and input JSON when there is bad thumbnail URL.
@@ -68,24 +95,15 @@ def test_download_preview():
         URL_FIELD_NAME:"http://repository.clemson.edu/cgi-bin/thumbnail.exe?CISOROOT=/cfb&CISOPTR=1040"
     }
 
-    def get_file_path():
-        global thumbs_root
-        id = 'clemson__cfb004'
-        md5 = '84BA8BC32C4316A96FDC89F51BEB427D'
-        pinfo(get_thumbs_root())
-        path = get_thumbs_root()
-        for i in xrange(0,32,2):
-            path = os.path.join(path, md5[i:i+2])
-        path = os.path.join(path, id+".jpg")
-        return path
     
     INPUT = json.dumps(GOOD_DATA)
-    GOOD_DATA[URL_FILE_PATH] = get_file_path()
+    GOOD_DATA[URL_FILE_PATH] = _get_file_path("clemson--cfb004", "jpg")
     EXPECTED_OUTPUT = json.dumps(GOOD_DATA)
     resp,content = H.request(url,"POST",body=INPUT,headers=HEADERS)
 
     assert resp.status == 200
     assert_same_jsons(EXPECTED_OUTPUT, content)
+
 
 def test_downloading_with_bad_URL():
     """
@@ -99,34 +117,30 @@ def test_downloading_with_bad_URL():
     assert resp.status == 200
     assert_same_jsons(INPUT, content)
 
-from dplaingestion.akamod.download_test_image import image_png, image_jpg
 
 def _check_downloading_image(extension, expected_content):
     """
-    TODO: comment here
+    Calls the download_preview service passing thumbnail URL to download_test_image
+    service, which 
     """
-    GOOD_DATA = { "id":"clemson--cfb004",
+    id = 'clemson_--__qkjwerjerj_akjdasdhwqe-399394'
+    GOOD_DATA = { "id":id,
             URL_FIELD_NAME: server() + 'download_test_image?extension=' + extension
     }
     INPUT = json.dumps(GOOD_DATA)
     resp, content = H.request(url, "POST", body=INPUT, headers=HEADERS)
-    filepath = get_thumbs_root() + "/84/BA/8B/C3/2C/43/16/A9/6F/DC/89/F5/1B/EB/42/7D/clemson__cfb004." + extension
+    # The file should be written at:
+    filepath = _get_file_path(id, extension)
     GOOD_DATA[URL_FILE_PATH] = filepath
     OUTPUT = json.dumps(GOOD_DATA)
     assert resp.status == 200
     assert_same_jsons(OUTPUT, content)
 
     # let's check if the files are the same
-    base64_downloaded_file = ""
     with open(filepath, "rb") as f:
         data = f.read()
-        print len(data)
         base64_downloaded_file = data.encode("base64").rstrip('\n')
-    print base64_downloaded_file[-10:] + "|"
-    print expected_content[-10:] + "|"
-    print len(base64_downloaded_file)
-    print len(expected_content)
-    assert base64_downloaded_file == expected_content
+        assert base64_downloaded_file == expected_content
 
 
 def test_dowloading_png_image():
@@ -134,6 +148,7 @@ def test_dowloading_png_image():
     Should return JSON with file path and download the file.
     """
     _check_downloading_image("png", image_png)
+
 
 def test_dowloading_jpg_image():
     """
