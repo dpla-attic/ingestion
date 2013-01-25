c2ca95cf67e0452a7f9ea5c7e1faf05495150362
Jeffrey Licht <jeff@podconsulting.com>
2013-01-25 12:05:53 -0500

    OAI mapping for new schema

--- lib/akamod/oai-to-dpla.py
+++ lib/akamod/oai-to-dpla.py
@@ -56,97 +56,6 @@ def spatial_transform(d):
         spatial.append(sp)
     return {"spatial":spatial}
 
-def created_transform(d):
-    created = {
-        "start": d["datestamp"],
-        "end": d["datestamp"]
-    }
-    return {"created":created}
-
-DATE_RANGE_RE = r'(\S+)\s*-\s*(\S+)'
-def split_date(d):
-    range = [robust_date_parser(x) for x in re.search(DATE_RANGE_RE,d).groups()]
-    return range
-
-DATE_8601 = '%Y-%m-%d'
-def robust_date_parser(d):
-    """
-    Robust wrapper around some date parsing libs, making a best effort to return
-    a single 8601 date from the input string. No range checking is performed, and
-    any date other than the first occuring will be ignored.
-
-    We use timelib for its ability to make at least some sense of invalid dates,
-    e.g. 2012/02/31 -> 2012/03/03
-
-    We rely only on dateutil.parser for picking out dates from nearly arbitrary
-    strings (fuzzy=True), but at the cost of being forgiving of invalid dates
-    in those kinds of strings.
-
-    Returns None if it fails
-    """
-    dd = None
-    try:
-        dd = dateutil_parse(d,fuzzy=True,default=DEFAULT_DATETIME)
-    except:
-        try:
-            dd = timelib.strtodatetime(d)
-        except ValueError:
-            pass
-
-    if dd:
-        ddiso = dd.isoformat()
-        return ddiso[:ddiso.index('T')]
-
-    return dateparser.to_iso8601(d.replace('ca.','').strip()) # simple cleanup prior to parse
-
-def parse_date_or_range(d):
-    if ' - ' in d: # FIXME could be more robust here, e.g. use year regex
-        a,b = split_date(d)
-    else:
-        parsed = robust_date_parser(d)
-        a,b = parsed,parsed
-    return a,b
-
-DATE_TESTS = {
-    "ca. July 1896": ("1896-07-01","1896-07-01"), # fuzzy dates
-    "1999.11.01": ("1999-11-01","1999-11-01"), # period delim
-    "2012-02-31": ("2012-03-02","2012-03-02"), # invalid date cleanup
-    "12-19-2010": ("2010-12-19","2010-12-19"), # M-D-Y
-    "5/7/2012": ("2012-05-07","2012-05-07"), # slash delim MDY
-    "1999 - 2004": ("1999-01-01","2004-01-01"), # year range
-    " 1999   -   2004  ": ("1999-01-01","2004-01-01"), # range whitespace
-}
-def test_parse_date_or_range():
-    for i in DATE_TESTS:
-        res = parse_date_or_range(i)
-        assert res == DATE_TESTS[i], "For input '%s', expected '%s' but got '%s'"%(i,DATE_TESTS[i],res)
-
-def temporal_transform(d):
-    temporal = []
-
-    # First look at the date field, and log any parsing errors
-    for t in (d["date"] if not isinstance(d["date"],basestring) else [d["date"]]):
-        a,b = parse_date_or_range(t)
-        if a is not None and b is not None:
-            temporal.append( {
-                "start": a,
-                "end": b
-            })
-        else:
-            logger.debug("Could not parse date: " + t)
-
-    # Then, check out the 'coverage' field, since dates may be there
-    if "coverage" in d:
-        for t in (d["coverage"] if not isinstance(d["coverage"],basestring) else [d["coverage"]]):
-            a,b = parse_date_or_range(t)
-            if a is not None and b is not None:
-                temporal.append( {
-                    "start": a,
-                    "end": b
-                })
-
-    return {"temporal":temporal}
-
 def is_shown_at_transform(d):
     source = ""
     for s in (d["handle"] if not isinstance(d["handle"],basestring) else [d["handle"]]):
@@ -165,11 +74,12 @@ def is_shown_at_transform(d):
 # item dict representing the new property and its value
 CHO_TRANSFORMER = {
     "contributor"      : lambda d: {"contributor": d.get("contributor",None)},
-    "coverage"         : spatial_transform,
+    "coverage"         : lambda d: {"spatial": d.get("coverage",None)},
     "creator"          : lambda d: {"creator": d.get("creator",None)},
     "description"      : lambda d: {"description": d.get("description",None)},
     "date"             : lambda d: {"date": d.get("date",None)},
     "language"         : lambda d: {"language": d.get("language",None)},
+    "format"           : lambda d: {"physicalmedium" : d.get("format",None)},
     "publisher"        : lambda d: {"publisher": d.get("publisher",None)},
     "relation"         : lambda d: {"relation": d.get("relation",None)},
     "rights"           : lambda d: {"rights": d.get("rights",None)},
