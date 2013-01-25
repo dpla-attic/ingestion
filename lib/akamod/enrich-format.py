8fc08206910dbd77a3c0693355ee1dcf8b454d6c
Jeffrey Licht <jeff@podconsulting.com>
2013-01-25 13:09:32 -0500

    Update enrichments to work with new schema

--- lib/akamod/enrich-format.py
+++ lib/akamod/enrich-format.py
@@ -7,29 +7,27 @@ import re
 
 @simple_service('POST', 'http://purl.org/la/dp/enrich-format', 'enrich-format', 'application/json')
 def enrichformat(body,ctype,action="enrich-format",prop="isShownAt/format",alternate="aggregatedCHO/physicalmedium"):
-    """
+    '''   
     Service that accepts a JSON document and enriches the "format" field of that document
     by: 
 
     a) setting the format to be all lowercase
     b) running through a set of cleanup regex's (e.g. image/jpg -> image/jpeg)
-    c) checking to see if the field is a valid IMT, and moving it to a separatee field if not
+    b) checking to see if the field is a valid IMT, and moving it to a separatee field if not
        See http://www.iana.org/assignments/media-types for list of valid media-types. We do not
-       require that a subtype be defined. 
-    d) Remove any extra text after the IMT   
+       require that a subtype be defined. The 
     
     By default works on the 'format' field, but can be overridden by passing the name of the field to use
     as the 'prop' parameter. Non-IMT's are moved the field defined by the 'alternate' parameter.
-    """
+    '''   
 
-    REGEXPS = ('image/jpg','image/jpeg'),('image/jp$', 'image/jpeg'), ('img/jpg', 'image/jpeg'), ('\W$','')
+    REGEXPS = ('image/jpg','image/jpeg'),('image/jp$', 'image/jpeg'), ('img/jpg', 'image/jpeg')
     IMT_TYPES = ['application','audio','image','message','model','multipart','text','video']
 
     def cleanup(s):
         s = s.lower().strip()
         for pattern, replace in REGEXPS:
             s = re.sub(pattern, replace, s)
-            s = re.sub(r"^([a-z0-9/]+)\s.*",r"\1",s)
         return s
 
     def is_imt(s):
@@ -38,7 +36,7 @@ def enrichformat(body,ctype,action="enrich-format",prop="isShownAt/format",alter
 
     try :
         data = json.loads(body)
-    except Exception:
+    except:
         response.code = 500
         response.add_header('content-type','text/plain')
         return "Unable to parse body as JSON"
@@ -46,16 +44,15 @@ def enrichformat(body,ctype,action="enrich-format",prop="isShownAt/format",alter
     if exists(data,prop):
         v = getprop(data,prop)
         format = []
-
-        physicalFormat = list(getprop(data,alternate)) if exists(data,alternate) else []
+        physicalFormat = []
         for s in (v if not isinstance(v,basestring) else [v]):
             format.append(cleanup(s)) if is_imt(cleanup(s)) else physicalFormat.append(s)
 
-        if format:
+        if len(format) > 0:
             setprop(data,prop,format[0]) if len(format) == 1 else setprop(data,prop,format)
         else:
             setprop(data,prop,None)
-        if physicalFormat:
+        if len(physicalFormat) > 0:
             setprop(data,alternate,physicalFormat[0]) if len(physicalFormat) == 1 else setprop(data,alternate,physicalFormat)
 
     return json.dumps(data)
--- lib/akamod/enrich-type.py
+++ lib/akamod/enrich-type.py
@@ -6,23 +6,20 @@ from dplaingestion.selector import getprop, setprop, exists
 import re
 
 @simple_service('POST', 'http://purl.org/la/dp/enrich-type', 'enrich-type', 'application/json')
-def enrichtype(body,ctype,action="enrich-type", prop="aggregatedCHO/type", alternate="aggregatedCHO/physicalmedium"):
-    """   
+def enrichtype(body,ctype,action="enrich-type",prop="aggregatedCHO/type"):
+    '''   
     Service that accepts a JSON document and enriches the "type" field of that document
     by: 
 
     a) making the type lowercase
     b) converting "image" to "still image" (TODO: Amy to confirm that this is ok)
-    c) applying a set of regexps to do data cleanup (remove plural forms)
-    d) moving all items that are not standard DC types to the physical format field (http://dublincore.org/documents/resource-typelist/)
+    b) applying a set of regexps to do data cleanup (remove plural forms)
     
     By default works on the 'type' field, but can be overridden by passing the name of the field to use
     as a parameter
-    """
+    '''   
 
     REGEXPS = ('images','image'), ('still image','image')
-    DC_TYPES = ['collection', 'dataset', 'event', 'image', 'still image', 'interactive resource', 'model', 'party', 'physical object',
-                'place', 'service', 'software', 'sound', 'text']
 
     def cleanup(s):
         s = s.lower().strip()
@@ -30,29 +27,19 @@ def enrichtype(body,ctype,action="enrich-type", prop="aggregatedCHO/type", alter
             s = re.sub(pattern, replace, s)
         return s
 
-    def is_dc_type(s):
-        return s in DC_TYPES
-
     try :
         data = json.loads(body)
-    except Exception:
+    except:
         response.code = 500
         response.add_header('content-type','text/plain')
         return "Unable to parse body as JSON"
 
     if exists(data,prop):
         v = getprop(data,prop)
-        dctype = []
-        physicalFormat = list(getprop(data,alternate)) if exists(data,alternate) else []
-
+        result = []
         for s in (v if not isinstance(v,basestring) else [v]):
-            dctype.append(cleanup(s)) if is_dc_type(cleanup(s)) else physicalFormat.append(s)
+            result.append(cleanup(s))
 
-        if dctype:
-            setprop(data,prop,dctype[0]) if len(dctype) == 1 else setprop(data,prop,dctype)
-        else:
-            setprop(data,prop,None)
-        if physicalFormat:
-            setprop(data,alternate,physicalFormat[0]) if len(physicalFormat) == 1 else setprop(data,alternate,physicalFormat)
+        setprop(data,prop,result[0]) if len(result) == 1 else setprop(data,prop,result)
 
     return json.dumps(data)
