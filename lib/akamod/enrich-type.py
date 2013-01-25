1c13dc52742d8b516cc2a2038755973ed2548305
Jeffrey Licht <jeff@podconsulting.com>
2013-01-25 13:09:32 -0500

    Update enrichments to work with new schema

--- lib/akamod/enrich-type.py
+++ lib/akamod/enrich-type.py
@@ -6,20 +6,23 @@ from dplaingestion.selector import getprop, setprop, exists
 import re
 
 @simple_service('POST', 'http://purl.org/la/dp/enrich-type', 'enrich-type', 'application/json')
-def enrichtype(body,ctype,action="enrich-type",prop="aggregatedCHO/type"):
-    '''   
+def enrichtype(body,ctype,action="enrich-type", prop="aggregatedCHO/type", alternate="aggregatedCHO/physicalmedium"):
+    """   
     Service that accepts a JSON document and enriches the "type" field of that document
     by: 
 
     a) making the type lowercase
     b) converting "image" to "still image" (TODO: Amy to confirm that this is ok)
-    b) applying a set of regexps to do data cleanup (remove plural forms)
+    c) applying a set of regexps to do data cleanup (remove plural forms)
+    d) moving all items that are not standard DC types to the physical format field (http://dublincore.org/documents/resource-typelist/)
     
     By default works on the 'type' field, but can be overridden by passing the name of the field to use
     as a parameter
-    '''   
+    """
 
     REGEXPS = ('images','image'), ('still image','image')
+    DC_TYPES = ['collection', 'dataset', 'event', 'image', 'still image', 'interactive resource', 'model', 'party', 'physical object',
+                'place', 'service', 'software', 'sound', 'text']
 
     def cleanup(s):
         s = s.lower().strip()
@@ -27,19 +30,29 @@ def enrichtype(body,ctype,action="enrich-type",prop="aggregatedCHO/type"):
             s = re.sub(pattern, replace, s)
         return s
 
+    def is_dc_type(s):
+        return s in DC_TYPES
+
     try :
         data = json.loads(body)
-    except:
+    except Exception:
         response.code = 500
         response.add_header('content-type','text/plain')
         return "Unable to parse body as JSON"
 
     if exists(data,prop):
         v = getprop(data,prop)
-        result = []
-        for s in (v if not isinstance(v,basestring) else [v]):
-            result.append(cleanup(s))
+        dctype = []
+        physicalFormat = list(getprop(data,alternate)) if exists(data,alternate) else []
 
-        setprop(data,prop,result[0]) if len(result) == 1 else setprop(data,prop,result)
+        for s in (v if not isinstance(v,basestring) else [v]):
+            dctype.append(cleanup(s)) if is_dc_type(cleanup(s)) else physicalFormat.append(s)
+
+        if dctype:
+            setprop(data,prop,dctype[0]) if len(dctype) == 1 else setprop(data,prop,dctype)
+        else:
+            setprop(data,prop,None)
+        if physicalFormat:
+            setprop(data,alternate,physicalFormat[0]) if len(physicalFormat) == 1 else setprop(data,alternate,physicalFormat)
 
     return json.dumps(data)
