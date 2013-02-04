d1e0c5f4917275c321b261e14db16f827c876022
Szymon Guz <simon@simononsoftware.com>
2013-02-04 18:20:53 +0100

    Fixed errors due to PEP8.

--- lib/akamod/lookup.py
+++ lib/akamod/lookup.py
@@ -3,17 +3,20 @@ from akara import response
 from akara.services import simple_service
 from amara.thirdparty import json
 
-@simple_service('POST', 'http://purl.org/la/dp/lookup', 'lookup', 'application/json')
+
+@simple_service('POST', 'http://purl.org/la/dp/lookup',
+    'lookup', 'application/json')
 def lookup(body, ctype, input_field, output_field, substitution):
     """
     Performs simple lookup.
     """
 
-    logger.debug("BODY  : [%s]"%body)
-    logger.debug("INPUT : [%s]"%input_field)
-    logger.debug("OUTPUT: [%s]"%output_field)
+    logger.debug("BODY  : [%s]" % body)
+    logger.debug("INPUT : [%s]" % input_field)
+    logger.debug("OUTPUT: [%s]" % output_field)
 
     LOG_JSON_ON_ERROR = True
+
     def log_json():
         if LOG_JSON_ON_ERROR:
             logger.debug(body)
@@ -52,29 +55,31 @@ def lookup(body, ctype, input_field, output_field, substitution):
     print(value)
 
     if isinstance(value, basestring):
-        logger.debug("Changing value of ['{0}':'{1}'] to {2}".format(input_field, value, d[value]))
+        logger.debug("Changing value of ['{0}':'{1}'] to {2}".
+                format(input_field, value, d[value]))
         data[output_field] = d[value]
 
     if isinstance(value, list):
-        logger.debug("Changing each value of array ['{0}':'{1}'] to array of values.".format(input_field, value))
+        msg = "Changing each value of array ['{0}':'{1}'] to array of values."
+        logger.debug(msg.format(input_field, value))
         outlist = []
         for v in value:
             if v in d:
                 logger.debug("Changing value of '{0}' to {1}".format(v, d[v]))
                 outlist.append(d[v])
             else:
-                logger.debug("Not changing value of '{0}', didn't find in {1}".format(v, substitution))
+                logger.debug("Not changing value of '{0}', didn't find in {1}".
+                        format(v, substitution))
                 outlist.append(v)
 
         data[output_field] = outlist
-
 
     return json.dumps(data)
 
 
 TEST_SUBSTITUTE = {
-    "aaa" : "AAA",
-    "bbb" : "BBB",
-    "ccc" : "CCC",
-    "ddd" : "DDD",
+    "aaa": "AAA",
+    "bbb": "BBB",
+    "ccc": "CCC",
+    "ddd": "DDD",
 }
