ca6d325611317f2b11bf63089c82d3948e2f5065
Alexey Rosolovskiy <arosolovskiy@thumbtack.net>
2013-02-04 17:21:07 +0700

    filter-empty-values module code cleaned, created unit test for the module

--- lib/akamod/filter-empty-values.py
+++ lib/akamod/filter-empty-values.py
@@ -21,141 +21,16 @@ HTTP_TYPE_TEXT = 'text/plain'
 HTTP_HEADER_TYPE = 'Content-Type'
 
 
-TEST_EXAMPLE = """{
-   "_id": "artstor--oai:oaicat.oclc.org:AKRESS_10310356231",
-   "_rev": "1-56c6a0d094f7642d53dc0f5fb7dc0c6b",
-   "description": "Full view; pre-restoration; Formerly in the Contini Bonacossi Collection, Florence.",
-   "rights": [
-       "",
-       "Please note that if this image is under copyright, you may need to contact one or more copyright owners for any use that is not permitted under the ARTstor Terms and Conditions of Use or not otherwise permitted by law. While ARTstor tries to update contact information, it cannot guarantee that such information is always accurate. Determining whether those permissions are necessary, and obtaining such permissions, is your sole responsibility."
-   ],
-   "dplaSourceRecord": {
-       "handle": [
-           "Thumbnail: http://media.artstor.net/imgstor/size2/kress/d0001/kress_1091_post_as.jpg",
-           "Image View: http://www.artstor.org/artstor/ViewImages?id=8DtZYyMmJloyLyw7eDt5QHgr&userId=gDBAdA%3D%3D",
-           "Ranking: 13000"
-       ],
-       "description": "Full view; pre-restoration; Formerly in the Contini Bonacossi Collection, Florence.",
-       "format": [
-           "41.5 x 23.3 cm",
-           "Tempera and gold leaf on poplar panel",
-           "panel "
-       ],
-       "rights": [
-           "",
-           "Please note that if this image is under copyright, you may need to contact one or more copyright owners for any use that is not permitted under the ARTstor Terms and Conditions of Use or not otherwise permitted by law. While ARTstor tries to update contact information, it cannot guarantee that such information is always accurate. Determining whether those permissions are necessary, and obtaining such permissions, is your sole responsibility."
-       ],
-       "label": "Madonna and Child with Saints",
-       "datestamp": "2011-11-07",
-       "creator": "Guariento d'Arpo, Italian, c. 1310-1368/70",
-       "coverage": [
-           "",
-           "Repository: North Carolina Museum of Art."
-       ],
-       "date": "c. 1360",
-       "title": "Madonna and Child with Saints",
-       "type": [
-           "Paintings",
-           "Painting"
-       ],
-       "id": "oai:oaicat.oclc.org:AKRESS_10310356231",
-       "subject": "Madonna and Child with Saints; Virgin and Child.; Francis, of Assisi, Saint, 1182-1226.; Giles, Saint, 8th century.; Virgin and Child--with Saint(s).; Jesus Christ.; Mary, Blessed Virgin, Saint.; Anthony, Abbot, Saint, ca. 251-356.; John, the Baptist, Saint."
-   },
-   "created": {
-       "start": "1360-01-01",
-       "end": "1360-01-01",
-       "displayDate": "c. 1360"
-   },
-   "ingestDate": "2013-02-02T01:12:27.659532",
-   "dplaContributor": {
-       "@id": "http://dp.la/api/contributor/artstor",
-       "name": "ARTstor OAICatMuseum"
-   },
-   "TBD_physicalformat": [
-       "41.5 x 23.3 cm",
-       "Tempera and gold leaf on poplar panel",
-       "panel"
-   ],
-   "creator": "Guariento d'Arpo, Italian, c. 1310-1368/70",
-   "title": "Madonna and Child with Saints",
-   "source": "Thumbnail: http://media.artstor.net/imgstor/size2/kress/d0001/kress_1091_post_as.jpg",
-   "spatial": [
-       {
-           "name": ""
-       },
-       {
-           "name": "Repository: North Carolina Museum of Art."
-       }
-   ],
-   "isPartOf": {
-       "@id": "http://dp.la/api/collections/artstor--SetDPLA",
-       "name": "SetDPLA",
-       "description": "SetDPLA"
-   },
-   "@context": {
-       "iso639": "dpla:iso639",
-       "@vocab": "http://purl.org/dc/terms/",
-       "end": {
-           "@id": "dpla:end",
-           "@type": "xsd:date"
-       },
-       "name": "xsd:string",
-       "dplaSourceRecord": "dpla:sourceRecord",
-       "dpla": "http://dp.la/terms/",
-       "coordinates": "dpla:coordinates",
-       "dplaContributor": "dpla:contributor",
-       "start": {
-           "@id": "dpla:start",
-           "@type": "xsd:date"
-       },
-       "state": "dpla:state",
-       "iso3166-2": "dpla:iso3166-2",
-       "LCSH": "http://id.loc.gov/authorities/subjects"
-   },
-   "ingestType": "item",
-   "@id": "http://dp.la/api/items/36a3ac936167a48e2f4ddf5c99f5cf1d",
-   "id": "36a3ac936167a48e2f4ddf5c99f5cf1d",
-   "subject": [
-       {
-           "name": "Madonna and Child with Saints"
-       },
-       {
-           "name": "Virgin and Child"
-       },
-       {
-           "name": "Francis, of Assisi, Saint, 1182-1226"
-       },
-       {
-           "name": "Giles, Saint, 8th century"
-       },
-       {
-           "name": "Virgin and Child--with Saint(s)"
-       },
-       {
-           "name": "Jesus Christ"
-       },
-       {
-           "name": "Mary, Blessed Virgin, Saint"
-       },
-       {
-           "name": "Anthony, Abbot, Saint, ca. 251-356"
-       },
-       {
-           "name": "John, the Baptist, Saint"
-       }
-   ]
-}
-"""
-
-
-def filter_empty_lives(d, ignore_keys=tuple()):
+def filter_empty_leaves(d, ignore_keys=tuple()):
     """
-    Removes keys with empty value from dictionary;
+    Removes empty leaves from dictionary tree; (Empty leaf = key without value;)
+
+    Ignores keys listed in ignore_keys sequence;
     Returns: cleaned dictionary;
     """
     def sub_cleaner(el):
         if isinstance(el, dict):
-            return filter_empty_lives(copy.deepcopy(el), ignore_keys)
+            return filter_empty_leaves(copy.deepcopy(el), ignore_keys)
         else:
             return el
 
@@ -163,7 +38,7 @@ def filter_empty_lives(d, ignore_keys=tuple()):
         if k in ignore_keys:
             continue
         elif isinstance(v, dict) and v:
-            d[k] = filter_empty_lives(v, ignore_keys)
+            d[k] = filter_empty_leaves(v, ignore_keys)
         elif isinstance(v, list) and v:
             d[k] = [sub_cleaner(e) for e in v if e]
         else:
@@ -172,20 +47,25 @@ def filter_empty_lives(d, ignore_keys=tuple()):
     return d
 
 def filter_empty_values(d, ignore_keys=tuple()):
+    """
+    Removes keys with empty values from dictionaries,
+    repeatedly runs leaves cleaner until all empty leaves are removed.
+    """
     hash_before = hash(str(d))
     hash_current = None
     while hash_before != hash_current:
         hash_before = hash_current
-        hash_current = hash(str(filter_empty_lives(d, ignore_keys)))
+        hash_current = hash(str(filter_empty_leaves(d, ignore_keys)))
     return d
 
 def test_filtering():
-    d = {"v1": "", "v2": "value2", "v3": {"vv1": "", "vv2": "v_value2"}, "v4": {}, "v5": {"0": {"name": ""}, "1": {"name": "name_value_1"}}, "v6": ["", "vvalue6", {}, {"v_sub": ""}], "v7": [""]}
-    print d, hash(str(d))
-    print filter_empty_values(d), hash(str(d))
-    d2 = json.loads(TEST_EXAMPLE)
-    print d2, hash(str(d2))
-    print filter_empty_values(d2, ("dplaSourceRecord",)), hash(str(d2))
+    source = {"v1": "", "v2": "value2", "v3": {"vv1": "", "vv2": "v_value2"}, "v4": {}, "v5": {"0": {"name": ""}, "1": {"name": "name_value_1"}}, "v6": ["", "vvalue6", {}, {"v_sub": ""}], "v7": [""]}
+    expected = {"v2": "value2", "v3": {"vv2": "v_value2"}, "v5": {"1": {"name": "name_value_1"}}, "v6": ["vvalue6"]}
+    filtered = filter_empty_values(copy.deepcopy(source))
+    assert expected == filtered, "Expected dictionary does not equal to filtered"
+    #d2 = json.loads(TEST_EXAMPLE)
+    #print d2, hash(str(d2))
+    #print filter_empty_values(d2, ("dplaSourceRecord",)), hash(str(d2))
 
 @simple_service('POST', 'http://purl.org/la/dp/filter_empty_values', 'filter_empty_values', HTTP_TYPE_JSON)
 def filter_empty_values_endpoint(body, ctype, ignore_key="dplaSourceRecord"):
@@ -193,7 +73,7 @@ def filter_empty_values_endpoint(body, ctype, ignore_key="dplaSourceRecord"):
         assert ctype == HTTP_TYPE_JSON, "%s is not %s" % (HTTP_HEADER_TYPE, HTTP_TYPE_JSON)
         data = json.loads(body)
     except Exception as e:
-        error_text = "%s: %s" % (e.__class__.__name__, str(e))
+        error_text = "Bad JSON: %s: %s" % (e.__class__.__name__, str(e))
         logger.exception(error_text)
         response.code = HTTP_INTERNAL_SERVER_ERROR
         response.add_header(HTTP_HEADER_TYPE, HTTP_TYPE_TEXT)
@@ -204,9 +84,9 @@ def filter_empty_values_endpoint(body, ctype, ignore_key="dplaSourceRecord"):
     return json.dumps(data)
 
 
-def main(args):
-    test_filtering()
-
-if __name__ == "__main__":
-    main(sys.argv)
+#def main(args):
+#    test_filtering()
+#
+#if __name__ == "__main__":
+#    main(sys.argv)
 
--- test/server_support.py
+++ test/server_support.py
@@ -88,6 +88,7 @@ MODULES = [
     "dplaingestion.akamod.download_preview",
 
     "dplaingestion.akamod.download_test_image",
+    "dplaingestion.akamod.filter-empty-values",
     ]
 
 class download_preview:
