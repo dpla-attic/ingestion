950b600d99b2107b84531f59914b1176b1d1cb2d
Szymon Guz <simon@simononsoftware.com>
2013-02-05 15:37:37 +0100

    Adding dictionary support.

--- lib/akamod/lookup.py
+++ lib/akamod/lookup.py
@@ -4,12 +4,52 @@ from akara.services import simple_service
 from amara.thirdparty import json
 
 
+def split_name(name):
+    return name.split(".")
+
+
+def find_element(data, name):
+    el = data
+    for n in split_name(name):
+        el = el[n]
+    return el
+
+
+def convert_data(value, dict_name):
+    """
+    Converts given data using provided dict name.
+    """
+    d = globals()[dict_name]
+    result = None
+
+    if isinstance(value, basestring):
+        logger.debug("Changing value of ['{0}':'{1}'] to {2}".
+                format(input_field, value, d[value]))
+        result = d[value]
+
+    if isinstance(value, list):
+        msg = "Changing each value of array ['{0}':'{1}'] to array of values."
+        logger.debug(msg.format(input_field, value))
+        result = []
+        for v in value:
+            if v in d:
+                logger.debug("Changing value of '{0}' to {1}".format(v, d[v]))
+                result.append(d[v])
+            else:
+                logger.debug("Not changing value of '{0}', didn't find in {1}".
+                        format(v, substitution))
+                result.append(v)
+
+    return result
+
+
 @simple_service('POST', 'http://purl.org/la/dp/lookup',
     'lookup', 'application/json')
 def lookup(body, ctype, input_field, output_field, substitution):
     """
     Performs simple lookup.
     """
+
 
     logger.debug("BODY  : [%s]" % body)
     logger.debug("INPUT : [%s]" % input_field)
@@ -30,6 +70,9 @@ def lookup(body, ctype, input_field, output_field, substitution):
         response.code = 500
         response.add_header('content-type', 'text/plain')
         return msg
+
+    x = find_element(data, input_field)
+    logger.debug(x)
 
     if not output_field:
         msg = "There is not provided output field name."
@@ -52,7 +95,6 @@ def lookup(body, ctype, input_field, output_field, substitution):
 
     d = globals()[key]
     value = data[input_field]
-    print(value)
 
     if isinstance(value, basestring):
         logger.debug("Changing value of ['{0}':'{1}'] to {2}".
