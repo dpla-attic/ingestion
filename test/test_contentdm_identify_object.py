80bd9cad439905f389ae6bbab920cb28441332cf
Szymon Guz <szymon@endpoint.com>
2013-02-06 15:18:30 +0100

    Changed the contentdm-identify-object module for schemav3.

---
+++ test/test_contentdm_identify_object.py
@@ -0,0 +1,157 @@
+
+import sys
+from server_support import server, print_error_log
+
+from amara.thirdparty import httplib2
+import os
+from amara.thirdparty import json
+from dict_differ import DictDiffer, assert_same_jsons, pinfo
+from nose.tools import nottest
+############################################################################
+## CONTENTDM
+## TODO: move to another file
+
+CT_JSON = {"Content-Type": "application/json"}
+HEADERS = {
+            "Content-Type": "application/json",
+            "Context": "{}",
+          }
+
+H = httplib2.Http()
+
+
+def contentdm_url(rights_field="r", download="True"):
+    return server() + \
+            "contentdm-identify-object?rights_field=%s&download=%s" \
+            % (rights_field, download)
+
+
+def test_contentdm_identify_object_without_download():
+    """
+    Should add a thumbnail URL made of the source URL.
+    """
+    INPUT = {
+            u"something": "x",
+            u"somethink": "y",
+            u"originalRecord":
+                    {"handle":
+                        ["aaa", "http://repository.clemson.edu/u?/scp,104"]
+                    },
+            u"left": "right now!"
+    }
+    EXPECTED = {
+            u"something": "x",
+            u"somethink": "y",
+            u"originalRecord": {
+                "handle":
+                    ["aaa", "http://repository.clemson.edu/u?/scp,104"]
+                },
+            u"object": {
+                "@id": "http://repository.clemson.edu/cgi-bin/" +
+                        "thumbnail.exe?CISOROOT=/scp&CISOPTR=104",
+                "format": "",
+                "rights": "right now!"
+            },
+            u"admin": {u"object_status": "ignore"},
+            u"left": "right now!"
+    }
+    url = contentdm_url(u"left", "False")
+    resp, content = H.request(url, "POST",
+            body=json.dumps(INPUT), headers=HEADERS)
+    print_error_log()
+    assert str(resp.status).startswith("2")
+    result = json.loads(content)
+
+    assert_same_jsons(EXPECTED, result)
+
+
+def test_contentdm_identify_object_with_download():
+    """
+    Should add a thumbnail URL made of the source URL.
+    """
+    INPUT = {
+            u"something": "x",
+            u"somethink": "y",
+            u"originalRecord": {
+                "handle": ["aaa", "http://repository.clemson.edu/u?/scp,104"]
+                },
+            u"left": "right now!"
+    }
+    EXPECTED = {
+            u"something": "x",
+            u"somethink": "y",
+            u"originalRecord": {
+                "handle": ["aaa", "http://repository.clemson.edu/u?/scp,104"]
+                },
+            u"object": {
+                "@id": "http://repository.clemson.edu/cgi-bin/" +
+                    "thumbnail.exe?CISOROOT=/scp&CISOPTR=104",
+                "format": "",
+                "rights": "right now!"
+            },
+            u"admin": {u"object_status": "pending"},
+            u"left": "right now!"
+    }
+    url = contentdm_url(u"left", "True")
+
+    resp, content = H.request(url, "POST",
+            body=json.dumps(INPUT), headers=HEADERS)
+
+    assert str(resp.status).startswith("2")
+    result = json.loads(content)
+
+    assert_same_jsons(EXPECTED, result)
+
+
+def test_contentdm_identify_object_bad_json():
+    """
+    Should get 500 from akara for bad json.
+    """
+    INPUT = ["{...}", "{aaa:'bbb'}", "xxx"]
+    for i in INPUT:
+        url = contentdm_url()
+        resp, ontent = H.request(url, "POST", body=i, headers=HEADERS)
+        assert resp.status == 500
+
+
+def test_contentdm_identify_object_missing_source_field():
+    """
+    Should return original JSON if the thumbnail URL field is missing.
+    """
+    INPUT = {
+            "aaa": "bbb",
+        }
+    INPUT = json.dumps(INPUT)
+    url = contentdm_url()
+    resp, content = H.request(url, "POST", body=INPUT, headers=HEADERS)
+
+    assert_same_jsons(INPUT, content)
+
+
+def test_contentdm_identify_object_bad_url():
+    """
+    Should return original JSON for bad URL.
+    """
+    bad_urls = [u"http://repository.clemson.edu/uscp104",
+        u"http://repository.clemson.edu/s?/scp,04",
+        u"http://repository.clemson.edu/u/scp,04",
+        u"http://repository.clemson.edu/u?/scp104",
+        u"http://repository.clemson.edu/u?/scp",
+        u"http://repository.clemson.edu/",
+            ]
+    INPUT = {
+            u"something": u"x",
+            u"somethink": u"y",
+            u"source": u""
+    }
+    for bad_url in bad_urls:
+        INPUT[u"source"] = bad_url
+        url = contentdm_url()
+        resp, content = H.request(url, "POST",
+                body=json.dumps(INPUT), headers=HEADERS)
+        assert str(resp.status).startswith("2")
+        assert_same_jsons(INPUT, content)
+
+
+if __name__ == "__main__":
+    raise SystemExit("Use nosetests")
