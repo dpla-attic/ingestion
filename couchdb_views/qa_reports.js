9096e11adf8fcbf9c0382cef311b8dc6bcaa735a
Miguel Alatorre <miguel@endpoint.com>
2013-02-14 10:37:18 -0500

    Updated qa_reports for schemav3.

--- couchdb_views/qa_reports.js
+++ couchdb_views/qa_reports.js
@@ -1,38 +1,80 @@
 {
-    "_id": "_design/lists",
-	"language": "javascript",
-	"views": {
-	"format": {
-	    "map": "function(doc) {\n  if (doc.ingestType == 'item') {\n\temit(null, doc.format);\n  }\n}"
-		},
-	    "format_count": {
-		"map": "function(doc) {\n  if (doc.ingestType == 'item') {\n\temit(doc.format, 1);\n  }\n}",
-		    "reduce": "_count"
-		    },
-		"type": {
-		    "map": "function(doc) {\n  if (doc.ingestType == 'item') {\n\temit(null, doc.type);\n  }\n}"
-			},
-		    "type_count": {
-			"map": "function(doc) {\n  if (doc.ingestType == 'item') {\n\temit(doc.type, 1);\n  }\n}",
-			    "reduce": "_count"
-			    },
-			"spatial": {
-			    "map": "function(doc) {\n  if (doc.ingestType == 'item') {\n\tfor (var i = 0; i < doc.spatial.length; i++) \n\t{\n\t\temit(doc.id , doc.spatial[i].name);\n\t}\n  }\n}"
-				},
-			    "spatial_count": {
-				"map": "function(doc) {\n  if (doc.ingestType == 'item') {\n\tfor (var i = 0; i < doc.spatial.length; i++) \n\t{\n\t\temit(doc.spatial[i].name, 1);\n\t}\n  }\n}",
-				    "reduce": "_count"
-				    },
-				"subject": {
-				    "map": "function(doc) {\n  if (doc.ingestType == 'item') {\n\tfor (var i = 0; i < doc.subject.length; i++) \n\t{\n\t\temit(doc.id , doc.subject[i].name);\n\t}\n  }\n}"
-					},
-				    "subject_count": {
-					"map": "function(doc) {\n  if (doc.ingestType == 'item') {\n\tfor (var i = 0; i < doc.subject.length; i++) \n\t{\n\t\temit(doc.subject[i].name, 1);\n\t}\n  }\n}",
-					    "reduce": "_count"
-					    }
-    },
-	"lists": {
-	    "csv": "function(head, req) { start({'headers': {'Content-Type': 'text/csv'}});var row; while (row = getRow()) { send(row.id + ',' + row.value + '\\n'); }}",
-		"count_csv": "function(head, req) { start({'headers': {'Content-Type': 'text/csv'}});var row; while (row = getRow()) { send(row.key + ',' + row.value + '\\n'); }}"
-		}
+   "_id": "_design/lists",
+   "language": "javascript",
+   "views": {
+       "title": {
+           "map": "function(doc) { if (doc.ingestType == 'item') { title = doc.aggregatedCHO.title; if (title.constructor.toString().indexOf('Array') == -1) { title = new Array(title); } for (i=0; i<title.length; i++) { emit(doc['id'], title[i]); }}}"
+       },
+       "title_count": {
+           "map": "function(doc) { if (doc.ingestType == 'item') { title = doc.aggregatedCHO.title; if (title.constructor.toString().indexOf('Array') == -1) { title = new Array(title); } for (i=0; i<title.length; i++) { emit(title[i],1); }}}",
+           "reduce": "_count"
+       },
+       "creator": {
+           "map": "function(doc) { if (doc.ingestType == 'item') { creator = doc.aggregatedCHO.creator; if (creator.constructor.toString().indexOf('Array') == -1) { creator = new Array(creator); } for (i=0; i<creator.length; i++) { emit(doc['id'], creator[i]);}}}"
+       },
+       "creator_count": {
+           "map": "function(doc) { if (doc.ingestType == 'item') { creator = doc.aggregatedCHO.creator; if (creator.constructor.toString().indexOf('Array') == -1) { creator = new Array(creator); } for (i=0; i<creator.length; i++) { emit(creator[i],1);}}}",
+           "reduce": "_count"
+       },
+       "publisher": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {pub = doc.aggregatedCHO.publisher;if (pub.constructor.toString().indexOf('Array') == -1) { pub = new Array(pub); }for (i=0; i<pub.length; i++) {emit(doc['id'], pub[i]);}}}"
+       },
+       "publisher_count": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {pub = doc.aggregatedCHO.publisher;if (pub.constructor.toString().indexOf('Array') == -1) { pub = new Array(pub); }for (i=0; i<pub.length; i++) {emit(pub[i],i);}}}",
+           "reduce": "_count"
+       },
+       "dates": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {d = doc.aggregatedCHO.date;if (d.constructor.toString().indexOf('Array') == -1) { d = new Array(d); }for (i=0; i<d.length; i++) {emit(doc['id'], d[i]);}}}"
+       },
+       "dates_count": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {d = doc.aggregatedCHO.date;if (d.constructor.toString().indexOf('Array') == -1) { d = new Array(d); }for (i=0; i<d.length; i++) {emit(d[i],1);}}}",
+           "reduce": "_count"
+       },
+       "description": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {desc = doc.aggregatedCHO.description;if (desc.constructor.toString().indexOf('Array') == -1) { desc = new Array(desc); }for (i=0; i<desc.length; i++) {emit(doc['id'], desc[i]);}}}"
+       },
+       "description_count": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {desc = doc.aggregatedCHO.description;if (desc.constructor.toString().indexOf('Array') == -1) { desc = new Array(desc); }for (i=0; i<desc.length; i++) {emit(desc[i],1);}}}",
+           "reduce": "_count"
+       },
+       "format": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {format = doc.aggregatedCHO.physicalmedium;if (format.constructor.toString().indexOf('Array') == -1) { format = new Array(format); }for (i=0; i<format.length; i++) {emit(doc['id'], format[i]);}}}"
+       },
+       "format_count": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {format = doc.aggregatedCHO.physicalmedium;if (format.constructor.toString().indexOf('Array') == -1) { format = new Array(format); }for (i=0; i<format.length; i++) {emit(format[i],1);}}}",
+           "reduce": "_count"
+       },
+       "type": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {t = doc.aggregatedCHO.type;if (t.constructor.toString().indexOf('Array') == -1) { t = new Array(t); }for (i=0; i<t.length; i++) {emit(doc['id'], t[i]);}}}"
+       },
+       "type_count": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {t = doc.aggregatedCHO.type;if (t.constructor.toString().indexOf('Array') == -1) { t = new Array(t); }for (i=0; i<t.length; i++) {emit(t[i],1);}}}",
+           "reduce": "_count"
+       },
+       "subject": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {sub = doc.aggregatedCHO.subject;if (sub.constructor.toString().indexOf('Array') == -1) { sub = new Array(sub); }for (i=0; i<sub.length; i++) {emit(doc['id'], sub[i].name);}}}"
+       },
+       "subject_count": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {sub = doc.aggregatedCHO.subject;if (sub.constructor.toString().indexOf('Array') == -1) { sub = new Array(sub); }for (i=0; i<sub.length; i++) {emit(sub[i].name,1);}}}",
+           "reduce": "_count"
+       },
+       "state": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {spatial = doc.aggregatedCHO.spatial;if (spatial.constructor.toString().indexOf('Array') == -1) { spatial = new Array(spatial); }for (i=0; i<spatial.length; i++) {if ('state' in spatial[i]) {emit(doc['id'], spatial[i]['state'])};}}}"
+       },
+       "state_count": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {spatial = doc.aggregatedCHO.spatial;if (spatial.constructor.toString().indexOf('Array') == -1) { spatial = new Array(spatial); }for (i=0; i<spatial.length; i++) {if ('state' in spatial[i]) {emit(spatial[i]['state'],1)};}}}",
+           "reduce": "_count"
+       },
+       "collection": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {cname = doc.collection.name;emit(doc['id'], cname);}}"
+       },
+       "collection_count": {
+           "map": "function(doc) {if (doc.ingestType == 'item') {cname = doc.collection.name;emit(cname,1);}}",
+           "reduce": "_count"
+       }
+   },
+   "lists": {
+       "csv": "function(head, req) { start({'headers': {'Content-Type': 'text/csv'}});var row; while (row = getRow()) { send(row.key + ',' + row.value + '\\n'); }}",
+       "count_csv": "function(head, req) { start({'headers': {'Content-Type': 'text/csv'}});var row; while (row = getRow()) { send(row.key + ',' + row.value + '\\n'); }}"
+   }
 }
