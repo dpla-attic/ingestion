4b2739b07aa41883b1686c4d96ef2c856778257d
Aleksey Rosolovskiy <aleksey@MacBook-Air-Aleksey.local>
2013-03-15 03:21:28 +0700

    mods fields mapper, initial version

--- lib/akamod/mods_to_dpla.py
+++ lib/akamod/mods_to_dpla.py
@@ -11,6 +11,11 @@ from zen import dateparser
 from dateutil.parser import parse as dateutil_parse
 import timelib
 
+from dplaingestion.selector import getprop as selector_getprop, exists
+
+
+def getprop(d, p):
+    selector_getprop(d, p, True)
 
 CONTEXT = {
     "@vocab": "http://purl.org/dc/terms/",
@@ -41,49 +46,77 @@ CONTEXT = {
     "name": "xsd:string"
 }
 
-def is_shown_at_transform(d):
-    source = ""
-    for s in (d["handle"] if not isinstance(d["handle"],basestring) else [d["handle"]]):
-        if is_absolute(s):
-            source = s
-            break
-    return {
-        "isShownAt" : {
-            "@id" : source,
-            "format" : d.get("format",None)
-        }
-    }
+def physical_description_handler(d, p):
+    note = getprop(d, p)
+    out = {}
+    for _dict in note:
+        if isinstance(_dict, dict) and "displayLabel" in _dict:
+            if _dict["displayLabel"] == "size inches":
+                out["extent"] = _dict["#text"]
+            if _dict["displayLabel"] == "condition":
+                out["description"] = _dict["#text"]
+    return out
+
+def subject_handler(d, p):
+    orig_subject = getprop(d, p)
+    subjects = {"subject": []}
+    for _dict in orig_subject:
+        if "topic" in _dict:
+            subjects["subject"].append(_dict["topic"])
+        if "name" in _dict:
+            subjects["subject"].append(getprop(_dict, "name/namePart"))
+    return subjects
+
+def location_handler(d, p):
+    location = getprop(d, p)
+    format = getprop(d, "physicalDescription/internetMediaType", True)
+    out = {}
+    try:
+        for _dict in location:
+            if "url" in _dict:
+                for url_dict in _dict["url"]:
+                    if url_dict and "access" in url_dict:
+                        if url_dict["access"] == "object in context":
+                            out["isShownAt"] = {"@id:": url_dict["#text"], "format": format}
+                        if url_dict["access"] == "preview":
+                            out["hasView"] = {"@id:": url_dict["#text"], "format": format}
+                            out["object"] = {"@id:": url_dict["#text"], "format": format}
+            if "physicalLocation" in _dict and isinstance(_dict["physicalLocation"], basestring):
+                out["dataProvider"] = _dict["physicalLocation"]
+    except Exception as e:
+        logger.error(e)
+    finally:
+        return out
+
+
+
 
 # Structure mapping the original property to a function returning a single
 # item dict representing the new property and its value
 CHO_TRANSFORMER = {
-    "contributor"      : lambda d: {"contributor": d.get("contributor",None)},
-    "coverage"         : lambda d: {"spatial": d.get("coverage",None)},
-    "creator"          : lambda d: {"creator": d.get("creator",None)},
-    "description"      : lambda d: {"description": d.get("description",None)},
-    "date"             : lambda d: {"date": d.get("date",None)},
-    "language"         : lambda d: {"language": d.get("language",None)},
-    "publisher"        : lambda d: {"publisher": d.get("publisher",None)},
-    "relation"         : lambda d: {"relation": d.get("relation",None)},
-    "rights"           : lambda d: {"rights": d.get("rights",None)},
-    "subject"          : lambda d: {"subject": d.get("subject",None)},
-    "title"            : lambda d: {"title": d.get("title",None)},
-    "type"             : lambda d: {"type": d.get("type",None)}
+    "recordInfo/languageOfCataloging/languageTerm/#text": lambda d, p: {"language": getprop(d, p)},
+    "name/namePart": lambda d, p: {"creator": getprop(d, p)},
+    "physicalDescription/note": physical_description_handler,
+    "physicalDescription/form/authority": lambda d, p: {"format": getprop(d, p)},
+    "originInfo/place/placeTerm": lambda d, p: {"spatial": getprop(d, p)},
+    "accessCondition": lambda d, p: {"rights": [s["#text"] for s in getprop(d, p) if "#text" in s]},
+    "subject": subject_handler,
+    "titleInfo/title": lambda d, p: {"title": getprop(d, p)},
+    "typeOfResource/#text": lambda d, p: {"type": getprop(d, p)},
+    "originInfo/dateCreated": lambda d, p: {"date": getprop(d, p)}
 }
 
 AGGREGATION_TRANSFORMER = {
-    "collection"       : lambda d: {"collection": d.get("collection",None)},
-    "id"               : lambda d: {"id": d.get("id",None), "@id" : "http://dp.la/api/items/"+d.get("id","")},
-    "_id"              : lambda d: {"_id": d.get("_id",None)},
-    "handle"           : is_shown_at_transform,
-    "originalRecord"   : lambda d: {"originalRecord": d.get("originalRecord",None)},
-    "source"           : lambda d: {"dataProvider": d.get("source",None)},
-
-    "ingestType"       : lambda d: {"ingestType": d.get("ingestType",None)},
-    "ingestDate"       : lambda d: {"ingestDate": d.get("ingestDate",None)}
+    "id"                         : lambda d, p: {"id": getprop(d, p), "@id" : "http://dp.la/api/items/"+getprop(d, p)},
+    "_id"                        : lambda d, p: {"_id": getprop(d, p)},
+    "originalRecord"             : lambda d, p: {"originalRecord": getprop(d, p)},
+    "ingestType"                 : lambda d, p: {"ingestType": getprop(d, p)},
+    "ingestDate"                 : lambda d, p: {"ingestDate": getprop(d, p)},
+    "location": location_handler,
+    "recordInfo/recordContentSource": lambda d, p: {"provider": getprop(d, p)}
 }
 
-@simple_service('POST', 'http://purl.org/la/dp/mods-to-dpla', 'mods_to_dpla', 'application/ld+json')
+@simple_service('POST', 'http://purl.org/la/dp/mods-to-dpla', 'mods-to-dpla', 'application/ld+json')
 def mods_to_dpla(body, ctype, geoprop=None):
     """
     Convert output of JSON-ified MODS/METS format into the DPLA JSON-LD format.
@@ -106,12 +139,15 @@ def mods_to_dpla(body, ctype, geoprop=None):
         "aggregatedCHO" : {}
     }
 
-    # Apply all transformation rules from original document to aggregatedCHO
-    for p in data.keys():
-        if p in CHO_TRANSFORMER:
-            out['aggregatedCHO'].update(CHO_TRANSFORMER[p](data))
-        if p in AGGREGATION_TRANSFORMER:
-            out.update(AGGREGATION_TRANSFORMER[p](data))
+    # Apply all transformation rules from original document
+    for p in CHO_TRANSFORMER:
+        if exists(data, p):
+            out["aggregatedCHO"].update(CHO_TRANSFORMER[p](data, p))
+    for p in AGGREGATION_TRANSFORMER:
+        if exists(data, p):
+            out.update(AGGREGATION_TRANSFORMER[p](data, p))
+    if "spatial" in out["aggregatedCHO"]:
+        out["aggregatedCHO"]["spatial"]["currentLocation"] = "Virginia"
 
     # Additional content not from original document
 
