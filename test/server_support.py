2e0f861502cf9f05808a11816dd1c0585839881d
Aleksey Rosolovskiy <aleksey@MacBook-Air-Aleksey.local>
2013-03-26 14:00:18 +0700

    nypl identify object module

--- test/server_support.py
+++ test/server_support.py
@@ -119,8 +119,10 @@ MODULES = [
     "dplaingestion.akamod.sets_prop",
     "dplaingestion.akamod.enrich_language",
     "dplaingestion.akamod.dpla-get-record",
+    "dplaingestion.akamod.mods_to_dpla",
     "dplaingestion.akamod.artstor_cleanup",
-    "dplaingestion.akamod.uiuc_publisher_to_dataprovider"
+    "dplaingestion.akamod.uiuc_publisher_to_dataprovider",
+    "dplaingestion.akamod.nypl_identify_object"
     ]
 
 class download_preview:
