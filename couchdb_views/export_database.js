21c4ccab63f8b81bc2af671835043f36480e26f0
Szymon Guz <szymon@endpoint.com>
2013-04-16 12:25:55 +0200

    Added export_database script.

---
+++ couchdb_views/export_database.js
@@ -0,0 +1,9 @@
+{
+    "language": "javascript",
+    "views": {
+        "all_source_names": {
+            "map": "function(doc) { var n = doc._id.split(\"--\"); emit(n[0], null);}",
+            "reduce": "_count"
+        }
+    }
+}
