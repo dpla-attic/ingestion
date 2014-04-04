8dfb84211a8daf20b2c62d7dc4cdb5ca5c2ca5ea
Mark Breedlove <mb@dp.la>
2014-04-04 16:07:52 -0400

    Added "moving image" to type conversion keywords.
    Hotfix; incremented version patchlevel.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ out.close()
 from distutils.core import setup
 
 setup( name = 'ingestion',
-       version = '22.1',
+       version = '22.1.1',
        description='DPLA Ingestion Subsystem',
        author='Mark Breedlove',
        author_email='mb@dp.la',
