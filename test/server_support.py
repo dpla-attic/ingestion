19f6aa362f0df5335d1e7e3cb7776cd6a6d46437
Mark Breedlove <mb@dp.la>
2014-04-17 09:03:05 -0400

    Modifid server_support to leave test Akara dir. behind for logs, etc.

--- test/server_support.py
+++ test/server_support.py
@@ -1,6 +1,5 @@
 """Support module to start a local Akara test server"""
 
-import atexit
 import os
 from os.path import abspath, dirname
 import shutil
@@ -99,6 +98,7 @@ class Akara:
   InternalServerRoot = 'http://localhost:%(port)s/'
   Listen = 'localhost:%(port)s'
   LogLevel = 'DEBUG'
+  ErrorLog = 'logs/error.log'
   MinSpareServers = 3
   # These affect test_server.py:test_restart
   MaxServers = 5
@@ -356,8 +356,6 @@ def remove_server_dir():
         else:
             print "Test server configuration and log files are in", config_root
         config_root = None
-
-atexit.register(remove_server_dir)
 
 # Start a new Akara server in server mode.
 def start_server():
