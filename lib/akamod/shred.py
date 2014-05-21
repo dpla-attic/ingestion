3326501f72575b726b636882be740b53f064f085
Miguel Alatorre <miguel@endpoint.com>
2014-05-21 09:59:15 -0500

    Fix for 4251
    - Add single_parens check to shred module

--- lib/akamod/shred.py
+++ lib/akamod/shred.py
@@ -26,6 +26,9 @@ def shred(body, ctype, action="shred", prop=None, delim=';', keepdup=None):
         response.add_header('content-type', 'text/plain')
         return "Unable to parse body as JSON\n" + str(e)
 
+    def single_parens(s):
+        return (s.count("(") == 0 or s.count(")") == 0)
+
     def mismatch_parens(s):
         return s.count("(") != s.count(")")
 
@@ -47,11 +50,12 @@ def shred(body, ctype, action="shred", prop=None, delim=';', keepdup=None):
 
                 shredded = [""]
                 for s in re.split(re.escape(delim), v):
-                    if mismatch_parens(shredded[-1]):
+                    if not single_parens(v) and mismatch_parens(shredded[-1]):
                         shredded[-1] += "%s%s" % (delim, s)
                     else:
                         shredded.append(s)
                 shredded = [i.strip() for i in shredded if i.strip()]
+ 
                 if not keepdup:
                     result = []
                     for s in shredded:
