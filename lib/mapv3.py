4f79aff41178e3b48c14adcca0c5b4fa3ede6fd9
Mark A. Matienzo <mark@dp.la>
2014-05-29 15:16:01 -0400

    Refactor validation process (refs #7289)
    Validation now will end up setting one of 3 values
    for admin.valid_after_enrich:
    
    * true - if instance validates
    * false - if instance does not validate
    * null - if something else unexpected happens or fails

--- lib/mapv3.py
+++ lib/mapv3.py
@@ -2,205 +2,242 @@ def array_or_item(obj):
     """Generate a Python list for use in JSON Schema oneOf declarations"""
     return [obj, {"$ref": "#/definitions/arrayMinOne", "items": obj}]
 
-MAPV3 = {
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "title": "DPLA Metadata Application Profile v3 JSON Schema",
-    # Do not include the ID until this can be dereferenced
-    # "id": "http://api.dp.la/schemas/mapv3#",
-    "type": "object",
-    # Recommended: store these at dereferenceable URIs
-    "oneOf": [
-        {"$ref": "#/definitions/item"},
-        {"$ref": "#/definitions/collection"}
-    ],
-    "definitions": {
-        "arrayMinOne": {
-            "type": "array",
-            "minItems": 1
-        },
-        "arrayOrString": {"oneOf": array_or_item({"type": "string"})},
-        "collection": {
-            "type": "object",
-            "required": ["@id", "title"],
-            "properties": {
-                "@context": {"type": ["string", "object"]},
-                "@id": {"type": "string", "format": "uri"},
-                "_id": {"type": "string"},
-                "_rev": {"type": "string"},
-                "admin": {"type": "object"},
-                "description": {"$ref": "#/definitions/arrayOrString"},
-                "ingestionSequence": {"type": "number"},
-                # do not check ingestDate as the 'date-time' json-schema
-                # format, since it requires RFC 3339 conformance and not
-                # ISO 8601 conformance
-                "ingestDate": {"type": "string"},
-                "ingestType": {"type": "string", "enum": ["collection"]},
-                "id": {"type": "string"},
-                "title": {"$ref": "#/definitions/arrayOrString"}
+MAPV3_SCHEMAS = {
+    "collection": {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "title": "DPLA Metadata Application Profile Collection v3 JSON Schema",
+        # Do not include the ID until this can be dereferenced
+        # "id": "http://api.dp.la/schemas/mapv3/collection#",
+        "type": "object",
+        "required": ["@id", "title"],
+        "properties": {
+            "@context": {"type": ["string", "object"]},
+            "@id": {"type": "string", "format": "uri"},
+            "_id": {"type": "string"},
+            "_rev": {"type": "string"},
+            "admin": {
+               "type": "object",
+               "additionalProperties": True
             },
-            "additionalProperties": False
+            "description": {"$ref": "#/definitions/arrayOrString"},
+            "ingestionSequence": {"type": "number"},
+            # do not check ingestDate as the 'date-time' json-schema
+            # format, since it requires RFC 3339 conformance and not
+            # ISO 8601 conformance
+            "ingestDate": {"type": "string"},
+            "ingestType": {"type": "string", "enum": ["collection"]},
+            "id": {"type": "string"},
+            "title": {"$ref": "#/definitions/arrayOrString"}
         },
-        "date": {
-            "type": "object",
-            "properties": {
-                "begin": { "type": ["string", "null"] },
-                "displayDate": { "type": ["string", "null"] },
-                "end": { "type": ["string", "null"] }
+        "additionalProperties": False,
+        "definitions": {
+            "arrayMinOne": {
+                "type": "array",
+                "minItems": 1
             },
-            "additionalProperties": False
-        },
-        "hasView": {
-            "type": "object",
-            "required": ["@id", "format"],
-            "properties": {
-                "@id": {"type": "string", "format": "uri"},
-                "format": {"type": ["string", "null"]},
-                "rights": {"$ref": "#/definitions/arrayOrString"}
+            "arrayOrString": {"oneOf": array_or_item({"type": "string"})},
+        }
+    },
+    "item": {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "title": "DPLA Metadata Application Profile Item v3 JSON Schema",
+        # Do not include the ID until this can be dereferenced
+        # "id": "http://api.dp.la/schemas/mapv3/item#",
+        "type": "object",
+        "required": [
+            "@context",
+            "@id",
+            "dataProvider",
+            "id",
+            "ingestionSequence",
+            "isShownAt",
+            "object",
+            "originalRecord",
+            "provider",
+            "sourceResource"
+        ],
+        "properties": {
+            "@context": {"type": ["string", "object"]},
+            "@id": {"type": "string", "format": "uri"},
+            "@type": {"type": "string", "enum": ["ore:Aggregation"]},
+            "_id": {"type": "string"},
+            "_rev": {"type": "string"},
+            "admin": {
+               "type": "object",
+               "additionalProperties": True
             },
-            "additionalProperties": False
+            "aggregatedCHO": {"type": "string", "format": "uri"},
+            "dataProvider": {"$ref": "#/definitions/arrayOrString"},
+            "hasView": {
+                "oneOf": array_or_item({"$ref": "#/definitions/hasView"})
+            },
+            "id": {"type": "string"},
+            "ingestionSequence": {"type": "number"},
+            # do not check ingestDate as the 'date-time' json-schema
+            # format, since it requires RFC 3339 conformance and not
+            # ISO 8601 conformance
+            "ingestDate": {"type": "string"},
+            "ingestType": {"type": "string", "enum": ["item"]},
+            "isShownAt": {"type": "string", "format": "uri"},
+            "object": {"type": "string", "format": "uri"},
+            "originalRecord": {
+                "type": "object",
+                "additionalProperties": True
+            },
+            "provider": {
+                "type": "object",
+                "required": ["@id", "name"],
+                "properties":{
+                    "@id": {"type": "string", "format": "uri"},
+                    "name": {"type": "string"}
+                }
+            },
+            "sourceResource": {"$ref": "#/definitions/sourceResource"}
         },
-        "item": {
-            "type": "object",
-            "required": [
-                "@context",
-                "@id",
-                "dataProvider",
-                "id",
-                "ingestionSequence",
-                "isShownAt",
-                "object",
-                "originalRecord",
-                "sourceResource"
-            ],
-            "properties": {
-                "@context": {"type": ["string", "object"]},
-                "@id": {"type": "string", "format": "uri"},
-                "@type": {"type": "string", "enum": ["ore:Aggregation"]},
-                "_id": {"type": "string"},
-                "_rev": {"type": "string"},
-                "admin": {"type": "object"},
-                "aggregatedCHO": {"type": "string", "format": "uri"},
-                "dataProvider": {"$ref": "#/definitions/arrayOrString"},
-                "hasView": {
-                    "oneOf": array_or_item({"$ref": "#/definitions/hasView"})
+        "additionalProperties": False,
+        "definitions": {
+            "arrayMinOne": {
+                "type": "array",
+                "minItems": 1
+            },
+            "arrayOrString": {"oneOf": array_or_item({"type": "string"})},
+            "collection": {
+                "type": "object",
+                "required": ["@id", "title"],
+                "properties": {
+                    "@context": {"type": ["string", "object"]},
+                    "@id": {"type": "string", "format": "uri"},
+                    "_id": {"type": "string"},
+                    "_rev": {"type": "string"},
+                    "admin": {
+                       "type": "object",
+                       "additionalProperties": True
+                    },
+                    "description": {"$ref": "#/definitions/arrayOrString"},
+                    "ingestionSequence": {"type": "number"},
+                    # do not check ingestDate as the 'date-time' json-schema
+                    # format, since it requires RFC 3339 conformance and not
+                    # ISO 8601 conformance
+                    "ingestDate": {"type": "string"},
+                    "ingestType": {"type": "string", "enum": ["collection"]},
+                    "id": {"type": "string"},
+                    "title": {"$ref": "#/definitions/arrayOrString"}
                 },
-                "id": {"type": "string"},
-                "ingestionSequence": {"type": "number"},
-                # do not check ingestDate as the 'date-time' json-schema
-                # format, since it requires RFC 3339 conformance and not
-                # ISO 8601 conformance
-                "ingestDate": {"type": "string"},
-                "ingestType": {"type": "string", "enum": ["item"]},
-                "isShownAt": {"type": "string", "format": "uri"},
-                "object": {"type": "string", "format": "uri"},
-                "originalRecord": {
-                    "type": "object",
-                    "additionalProperties": True
+                "additionalProperties": False
+            },
+            "date": {
+                "type": "object",
+                "properties": {
+                    "begin": { "type": ["string", "null"] },
+                    "displayDate": { "type": ["string", "null"] },
+                    "end": { "type": ["string", "null"] }
                 },
-                "provider": {
-                    "type": "object",
-                    "required": ["@id", "name"],
-                    "properties":{
-                        "@id": {"type": "string", "format": "uri"},
-                        "name": {"type": "string"}
-                    }
+                "additionalProperties": False
+            },
+            "hasView": {
+                "type": "object",
+                "required": ["@id", "format"],
+                "properties": {
+                    "@id": {"type": "string", "format": "uri"},
+                    "format": {"type": ["string", "null"]},
+                    "rights": {"$ref": "#/definitions/arrayOrString"}
                 },
-                "sourceResource": {"$ref": "#/definitions/sourceResource"}
-            }
-        },
-        "sourceResource": {
-            "type": "object",
-            "required": [
-                "rights", # Many records are still missing rights
-                "title"
-            ],
-            "additionalProperties": False,
-            "properties": {
-                "@id": {"type": "string", "format": "uri"},
-                "collection": {
-                    "oneOf": array_or_item({"$ref": "#/definitions/collection"})
-                },
-                "contributor": {"$ref": "#/definitions/arrayOrString"},
-                "creator": {"$ref": "#/definitions/arrayOrString"},
-                "date": {
-                    "oneOf": array_or_item({"$ref": "#/definitions/date"})
-                },
-                "description": {"$ref": "#/definitions/arrayOrString"},
-                "extent": {"$ref": "#/definitions/arrayOrString"},
-                "format": {"$ref": "#/definitions/arrayOrString"},
-                "identifier": {"$ref": "#/definitions/arrayOrString"},
-                "language": {
-                    "type": "array",
-                    "items": {
-                        "type": "object",
-                        "additionalProperties": False,
-                        "required": ["name"],
-                        "properties": {
-                            "name": { "type": "string" },
-                            "iso639_3": {
-                                "type": "string",
-                                "minLength": 3,
-                                "maxLength": 3
+                "additionalProperties": False
+            },
+            "sourceResource": {
+                "type": "object",
+                "required": [
+                    "rights", # Many records are still missing rights
+                    "title"
+                ],
+                "additionalProperties": False,
+                "properties": {
+                    "@id": {"type": "string", "format": "uri"},
+                    "collection": {
+                        "oneOf": array_or_item({"$ref": "#/definitions/collection"})
+                    },
+                    "contributor": {"$ref": "#/definitions/arrayOrString"},
+                    "creator": {"$ref": "#/definitions/arrayOrString"},
+                    "date": {
+                        "oneOf": array_or_item({"$ref": "#/definitions/date"})
+                    },
+                    "description": {"$ref": "#/definitions/arrayOrString"},
+                    "extent": {"$ref": "#/definitions/arrayOrString"},
+                    "format": {"$ref": "#/definitions/arrayOrString"},
+                    "identifier": {"$ref": "#/definitions/arrayOrString"},
+                    "language": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "additionalProperties": False,
+                            "required": ["name"],
+                            "properties": {
+                                "name": { "type": "string" },
+                                "iso639_3": {
+                                    "type": "string",
+                                    "minLength": 3,
+                                    "maxLength": 3
+                                }
                             }
                         }
-                    }
-                },
-                "publisher": {"$ref": "#/definitions/arrayOrString"},
-                "relation": {"$ref": "#/definitions/arrayOrString"},
-                "rights": {"$ref": "#/definitions/arrayOrString"},
-                "spatial": {
-                    "oneOf": array_or_item({"$ref": "#/definitions/spatial"})
-                },
-                "specType": {"$ref": "#/definitions/arrayOrString"},
-                "stateLocatedIn": {
-                    "type": "array",
-                    "items": {
-                        "type": "object",
-                        "properties": {
-                            "name": {"type": "string"}
+                    },
+                    "publisher": {"$ref": "#/definitions/arrayOrString"},
+                    "relation": {"$ref": "#/definitions/arrayOrString"},
+                    "rights": {"$ref": "#/definitions/arrayOrString"},
+                    "spatial": {
+                        "oneOf": array_or_item({"$ref": "#/definitions/spatial"})
+                    },
+                    "specType": {"$ref": "#/definitions/arrayOrString"},
+                    "stateLocatedIn": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "name": {"type": "string"}
+                            }
                         }
+                    },
+                    "subject": {
+                        "$ref": "#/definitions/arrayMinOne",
+                        "items": {
+                            "type": "object",
+                            "properties": {
+                                "name": {"type": "string"}
+                            },
+                        }
+                    },
+                    "temporal": {"$ref": "#/definitions/arrayOrString"},
+                    "title": {"$ref": "#/definitions/arrayOrString"},
+                    "type": {
+                        "oneOf": array_or_item({"$ref": "#/definitions/type"})
                     }
-                },
-                "subject": {
-                    "$ref": "#/definitions/arrayMinOne",
-                    "items": {
-                        "type": "object",
-                        "properties": {
-                            "name": {"type": "string"}
-                        },
-                    }
-                },
-                "temporal": {"$ref": "#/definitions/arrayOrString"},
-                "title": {"$ref": "#/definitions/arrayOrString"},
-                "type": {
-                    "oneOf": array_or_item({"$ref": "#/definitions/type"})
                 }
+            },
+            "spatial": {
+                "type": "object",
+                "properties": {
+                    "city": {"type": "string"},
+                    "coordinates": {"type": "string"},
+                    "country": {"type": "string"},
+                    "county": {"type": "string"},
+                    "name": {"type": "string"},
+                    "region": {"type": "string"},
+                    "state": {"type": "string"}
+                }
+            },
+            "type": {
+                "type": "string",
+                "enum": [
+                    "collection",
+                    "dataset",
+                    "image",
+                    "interactive resource",
+                    "moving image",
+                    "service",
+                    "sound",
+                    "text"
+                ]
             }
-        },
-        "spatial": {
-            "type": "object",
-            "properties": {
-                "city": {"type": "string"},
-                "coordinates": {"type": "string"},
-                "country": {"type": "string"},
-                "county": {"type": "string"},
-                "name": {"type": "string"},
-                "region": {"type": "string"},
-                "state": {"type": "string"}
-            }
-        },
-        "type": {
-            "type": "string",
-            "enum": [
-                "collection",
-                "dataset",
-                "image",
-                "interactive resource",
-                "moving image",
-                "service",
-                "sound",
-                "text"
-            ]
         }
     }
 }
