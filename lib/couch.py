dpla-attic/ingestion

commit d1b43d7269e1176c0e411dbf0806d89463d56712

Handle empty result in couch.Couch._get_bulk_download_doc()

lib/couch.py
@@ -244,10 +244,11 @@ class Couch(object):
         view_name = "all_docs/by_contributor"
         view = self.bulk_download_db.view(view_name, include_docs=True,
                                            key=contributor)
-
         try:
             return view.rows[0]["doc"]
-        except couchdb.http.ResourceNotFound:
+        except (couchdb.http.ResourceNotFound, IndexError):
+            # View does not exist (ResourceNotFound) or there's nothing for
+            # this contributor yet (IndexError).
             return {"contributor": contributor}
 
     def _prep_for_diff(self, doc):

commit cff01f33f6525065d95b7a4b5161bedac6f031f3

Revised recent change to couch.Couch._get_bulk_download_doc()
Commit d1b43d72 introduced an exception catch for an empty result.
Separate the response for a view not existing from that of an empty
result.  A view not existing is a more serious issue.

lib/couch.py
@@ -246,9 +246,11 @@ class Couch(object):
                                            key=contributor)
         try:
             return view.rows[0]["doc"]
-        except (couchdb.http.ResourceNotFound, IndexError):
-            # View does not exist (ResourceNotFound) or there's nothing for
-            # this contributor yet (IndexError).
+        except couchdb.http.ResourceNotFound:
+            raise Exception("View all_docs/by_contributor does not exist "
+                            "in bulk_download database.")
+        except IndexError:
+            # Nothing for this contributor yet, so start new document.
             return {"contributor": contributor}
 
     def _prep_for_diff(self, doc):