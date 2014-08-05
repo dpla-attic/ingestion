1097e044e9da096bd3fc7ee0f369a7143a67bdc1
Miguel Alatorre <miguel@endpoint.com>
2014-08-05 17:18:17 -0500

    Refactor enrich_language module to set language/iso639_3 and language/name from the language code
    - Remove cleanup_language module
    - Remove cleanup_language from profiles
    - Remove lookup from profiles when used for language mapping

--- lib/akamod/enrich_language.py
+++ lib/akamod/enrich_language.py
@@ -2,37 +2,78 @@ from akara import logger
 from akara import response
 from akara.services import simple_service
 from amara.thirdparty import json
-from dplaingestion.selector import getprop, setprop, exists
+from dplaingestion.selector import getprop, setprop, delprop, exists
+from dplaingestion.iso639_3 import ISO639_3_SUBST
+from dplaingestion.iso639_3 import EXACT_LANGUAGE_NAME_REGEXES
+from dplaingestion.iso639_3 import WB_LANGUAGE_NAME_REGEXES
+from dplaingestion.iso639_3 import ISO639_3_1
+from dplaingestion.iso639_1 import ISO639_1
 import re
 
-@simple_service('POST', 'http://purl.org/la/dp/enrich_language', 'enrich_language', 'application/json')
-def enrich_language(body, ctype, action="enrich_language", prop="sourceResource/language"):
-    '''
-    Service that accepts a JSON document and enriches the "language" field of that document
-    by:
+@simple_service("POST", "http://purl.org/la/dp/enrich_language",
+                "enrich_language", "application/json")
+def enrich_language(body, ctype, action="enrich_language",
+                      prop="sourceResource/language"):
+    """
+    Service that accepts a JSON document and sets the language ISO 639-3
+    code(s) and language name from the current language value(s) by:
 
-    a) converting a list of language values into list of dictionaries: {"name": language}
+    a) Checking if the value is a language code, else
+    a) Attempting to convert value the value from ISO 639-1 to ISO639-3, else
+    c) Attempting to find an exact language name match, else
+    d) Attempting to find language name matches withing the value
+    """
 
-    By default it works on the 'language' field, but can be overridden by passing the name of the field to use
-    as a parameter
-    '''
+    def iso1_to_iso3(s):
+        s = re.sub("[-_/].*$", "", s).strip()
+        return ISO639_1.get(s, s)
 
     try:
         data = json.loads(body)
     except:
         response.code = 500
-        response.add_header('content-type', 'text/plain')
+        response.add_header("content-type", "text/plain")
         return "Unable to parse body as JSON"
 
     if exists(data, prop):
-        langs = getprop(data, prop)
+        v = getprop(data, prop)
+        language_strings = [v] if not isinstance(v, list) else v
 
-        if isinstance(langs, basestring):
-            setprop(data, prop, {"name": langs})
-        elif isinstance(langs, list):
-            languages = []
-            for l in langs:
-                languages.append({"name": l})
-            setprop(data, prop, languages)
+        iso_codes = []
+        for lang_string in language_strings:
+            # Check if raw value is a code
+            if lang_string not in iso_codes and lang_string in ISO639_3_SUBST:
+                iso_codes.append(lang_string)
+            else:
+                # If lang_string is an ISO 639-1 code, convert to ISO 639-3
+                iso3 = iso1_to_iso3(
+                        re.sub("[\.\[\]\(\)]", "", lang_string).lower().strip()
+                        )
+                if iso3 not in iso_codes and iso3 in ISO639_3_SUBST:
+                    iso_codes.append(iso3)
+                else:
+                    # First check for exact language name matches
+                    for iso_code, regex in EXACT_LANGUAGE_NAME_REGEXES.items():
+                        match = regex.match(lang_string.strip())
+                        if match:
+                            iso_codes.append(iso_code)
+                            break
+
+                    if match is None:
+                        # Check for language names with word boundary regex
+                        for iso_code, regex in WB_LANGUAGE_NAME_REGEXES.items():
+                            if regex.search(lang_string):
+                                iso_codes.append(iso_code)
+
+        if iso_codes:
+            seen = set()
+            language = [{"iso639_3": code, "name": ISO639_3_SUBST[code]} for
+                        code in iso_codes if not
+                        (code in seen or seen.add(code))]
+            setprop(data, prop, language)
+        else:
+            logger.warning("Did not find language code in [%s] for record %s" %
+                           (language_strings, data["_id"]))
+            delprop(data, prop)
 
     return json.dumps(data)
--- test/test_enrich_language.py
+++ test/test_enrich_language.py
@@ -1,143 +1,145 @@
-import os
-import sys
-import re
-import hashlib
-from server_support import server, print_error_log, get_thumbs_root, H
-from dplaingestion.akamod.download_test_image import image_png, image_jpg
+# -*- coding: utf-8 -*-
+from server_support import server, H
 from amara.thirdparty import json
-from dict_differ import DictDiffer, assert_same_jsons, pinfo
-from nose.tools import nottest
-
-BASIC_URL = server() + "enrich_language"
+from dict_differ import assert_same_jsons
 
 def _get_server_response(body, prop=None):
     """
-Returns response from server using provided url.
-"""
-    url = BASIC_URL + "?"
-
-    if prop is not None:
-        url += "prop=%s&" % prop
-
+    Returns response from server using provided url.
+    """
+    url = server() + "enrich_language"
     return H.request(url, "POST", body=body)
 
-
-def test_bad_INPUT_json():
-    """
-Should return 500 when getting bad JSON.
-"""
-    INPUT = '{"aaabbb: eeee}'
-    resp, content = _get_server_response(INPUT, "a")
-    assert resp.status == 500
-
-
-def test_no_params():
-    """
-Should return the same JSON for no param and no default key.
-"""
-    INPUT = '{"aaa":"bbb"}'
-    resp, content = _get_server_response(INPUT)
-    assert resp.status == 200
-    assert_same_jsons(INPUT, content)
-
-
-def test_no_params_with_languages():
-    """
-Should return converted JSON for no param.
-"""
+def test_language_set_from_iso639_3_code():
+    """Should set language from ISO639-3 code"""
     INPUT = {
         "sourceResource": {
-            "language": "aaa"
+            "language": ["upv", "paf", "ntj", "ndd", "bcf"]
         }
     }
-    resp, content = _get_server_response(json.dumps(INPUT))
-    INPUT["sourceResource"]["language"] = {"name": "aaa"}
-    print_error_log()
-    assert resp.status == 200
-    assert_same_jsons(INPUT, content)
-
-
-def test_no_params_with_many_languages():
-    """
-Should return converted JSON for no param.
-"""
-    INPUT = {
-        "sourceResource": {
-            "language": ["aaa", "bbb"]
-        }
-    }
-    resp, content = _get_server_response(json.dumps(INPUT))
-    INPUT["sourceResource"]["language"] = [{"name": "aaa"}, {"name": "bbb"}]
-    assert resp.status == 200
-    assert_same_jsons(INPUT, content)
-
-def test_cleanup_enrich_then_lookup1():
-    """Should produce both name and iso639_3 language fields"""
-    INPUT = [
-        "en", "English", ["eng"], ["English"], ["en", "English"]
-    ]
     EXPECTED = {
         "sourceResource": {
-            "language": [{"name": "English", "iso639_3": "eng"}]
+            "language": [
+                {"iso639_3": "upv", "name": "Uripiv-Wala-Rano-Atchin"},
+                {"iso639_3": "paf", "name": u"Paranawát"},
+                {"iso639_3": "ntj", "name": "Ngaanyatjarra"},
+                {"iso639_3": "ndd", "name": "Nde-Nsele-Nta"},
+                {"iso639_3": "bcf", "name": "Bamu"}
+            ]
         }
     }
+    resp, content = _get_server_response(json.dumps(INPUT))
+    assert resp.status == 200
+    assert_same_jsons(EXPECTED, json.loads(content))
 
-    for i in range(len(INPUT)):
-        input = {"sourceResource": {"language": INPUT[i]}}
-        url = server() + "cleanup_language"
-        resp, content = H.request(url, "POST", json.dumps(input))
-        assert resp.status == 200
-        url = server() + "enrich_language"
-        resp, content = H.request(url, "POST", content)
-        assert resp.status == 200
-        url = server() + "lookup?prop=sourceResource%2Flanguage%2Fname" + \
-              "&target=sourceResource%2Flanguage%2Fname&substitution=iso639_3"
-        resp, content = H.request(url, "POST", content)
-        assert resp.status == 200
-        url = server() + "lookup?prop=sourceResource%2Flanguage%2Fname" + \
-                         "&target=sourceResource%2Flanguage%2Fiso639_3" + \
-                         "&substitution=iso639_3&inverse=True"
-        resp, content = H.request(url, "POST", content)
-        assert resp.status == 200
-        assert_same_jsons(content, EXPECTED)
-
-def test_cleanup_enrich_then_lookup2():
-    """Should produce both name and iso639_3 language fields"""
+def test_language_set_from_iso639_2_code():
+    """Should set language from ISO639-2 code"""
     INPUT = {
         "sourceResource": {
+            "language": ["aum", "mxy", "mxr", "frm", "ndi"]
+        }
+    }
+    EXPECTED = {
+        "sourceResource": {
             "language": [
-                "en", "French and arabic", "spanish Spanish", "Ze Germans"
+                {"iso639_3": "aum", "name": "Asu (Nigeria)"},
+                {"iso639_3": "mxy", "name": u"Southeastern Nochixtlán Mixtec"},
+                {"iso639_3": "mxr", "name": "Murik (Malaysia)"},
+                {"iso639_3": "frm", "name": "Middle French (ca. 1400-1600)"},
+                {"iso639_3": "ndi", "name": "Samba Leko"}
+            ]
+        }
+    }
+    resp, content = _get_server_response(json.dumps(INPUT))
+    assert resp.status == 200
+    assert_same_jsons(EXPECTED, json.loads(content))
+
+
+def test_language_set_from_iso639_1_code():
+    """Should convert ISO639-1 code to ISO639-3 code then set language"""
+    INPUT = {
+        "sourceResource": {
+            "language": ["ce", "fi", "ht", "ho", "id"]
+        }
+    }
+    EXPECTED = {
+        "sourceResource": {
+            "language": [
+                {"iso639_3": "che", "name": "Chechen"},
+                {"iso639_3": "fin", "name": "Finnish"},
+                {"iso639_3": "hat", "name": "Haitian"},
+                {"iso639_3": "hmo", "name": "Hiri Motu"},
+                {"iso639_3": "ind", "name": "Indonesian"}
+            ]
+        }
+    }
+    resp, content = _get_server_response(json.dumps(INPUT))
+    assert resp.status == 200
+    assert_same_jsons(EXPECTED, json.loads(content))
+
+def test_language_set_from_exact_regex_match():
+    """Should set language from ISO639-3 language name regex match"""
+    INPUT = {
+        "sourceResource": {
+            "language": ["mUlTiple languAges",
+                         "middle French (ca. 1400-1600)",
+                         "icelandic sign language"]
+        }
+    }
+    EXPECTED = {
+        "sourceResource": {
+            "language": [
+                {"iso639_3": "mul", "name": "Multiple languages"},
+                {"iso639_3": "frm", "name": "Middle French (ca. 1400-1600)"},
+                {"iso639_3": "icl", "name": "Icelandic Sign Language"}
             ]
         }
     }
 
+    resp, content = _get_server_response(json.dumps(INPUT))
+    assert resp.status == 200
+    assert_same_jsons(EXPECTED, json.loads(content))
+
+def test_language_set_from_word_boundary_regex_match():
+    """Should set language from splitting on whitespace and matching against
+       ISO639-1 langauge names
+    """
+    INPUT = {
+        "sourceResource": {
+            "language": ["Bananas English Spanish", "Apples German Spanish"]
+        }
+    }
     EXPECTED = {
         "sourceResource": {
             "language": [
-                {"name": "English", "iso639_3": "eng"},
-                {"name": "French", "iso639_3": "fre"},
-                {"name": "Arabic", "iso639_3": "ara"},
-                {"name": "Spanish", "iso639_3": "spa"}
+                {"iso639_3": "spa", "name": "Spanish"},
+                {"iso639_3": "eng", "name": "English"},
+                {"iso639_3": "ger", "name": "German"},
+                {"iso639_3": "deu", "name": "German"}
+            ]
+        }
+    }
+    resp, content = _get_server_response(json.dumps(INPUT))
+    assert resp.status == 200
+    assert_same_jsons(EXPECTED, json.loads(content))
+
+def test_language_set_from_unicode_regex_match():
+    INPUT = {
+        "sourceResource": {
+            "language": u"Southeastern Nochixtlán Mixtec"
+        }
+    }
+    EXPECTED = {
+        "sourceResource": {
+            "language": [
+                {"iso639_3": "mxy", "name": u"Southeastern Nochixtlán Mixtec"}
             ]
         }
     }
 
-    url = server() + "cleanup_language"
-    resp, content = H.request(url, "POST", json.dumps(INPUT))
+    resp, content = _get_server_response(json.dumps(INPUT))
     assert resp.status == 200
-    url = server() + "enrich_language"
-    resp, content = H.request(url, "POST", content)
-    assert resp.status == 200
-    url = server() + "lookup?prop=sourceResource%2Flanguage%2Fname" + \
-          "&target=sourceResource%2Flanguage%2Fname&substitution=iso639_3"
-    resp, content = H.request(url, "POST", content)
-    assert resp.status == 200
-    url = server() + "lookup?prop=sourceResource%2Flanguage%2Fname" + \
-                     "&target=sourceResource%2Flanguage%2Fiso639_3" + \
-                     "&substitution=iso639_3&inverse=True"
-    resp, content = H.request(url, "POST", content)
-    assert resp.status == 200
-    assert_same_jsons(content, EXPECTED)
+    assert_same_jsons(EXPECTED, json.loads(content))
 
 if __name__ == "__main__":
     raise SystemExit("Use nosetest")
