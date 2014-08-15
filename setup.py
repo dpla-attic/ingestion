9253d740dc38f6789a416756f7b05fec185ea9c0
Mark Breedlove <mb@dp.la>
2014-08-15 12:01:55 -0400

    Bumped version to 31.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ out.close()
 from distutils.core import setup
 
 setup( name = 'ingestion',
-       version = '31.1',
+       version = '31.2',
        description='DPLA Ingestion Subsystem',
        author='Digital Public Library of America',
        author_email='tech@dp.la',
