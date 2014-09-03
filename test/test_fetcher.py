ec6085c2520039633717288200ad0ac3fa337020
Mark A. Matienzo <mark@dp.la>
2014-09-03 18:01:25 -0400

    Undo some changes that shouldn't have been committed

--- test/test_fetcher.py
+++ test/test_fetcher.py
@@ -139,10 +139,7 @@ def test_absolute_url_fetcher_ia():
         break
 
 # Exclude the MWDL test in Travis as access to the feed is restricted
-# @attr(travis_exclude='yes', uses_network='yes')
-# TEMPORARY:  disable this test because the recent MWDL upgrade introduced
-#             data errors that have yet to be resolved.
-@nottest
+@attr(travis_exclude='yes', uses_network='yes')
 def test_absolute_url_fetcher_mwdl():
     profile_path = "profiles/mwdl.pjs"
     fetcher =  create_fetcher(profile_path, uri_base, config_file)
