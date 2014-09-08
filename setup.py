148ec7f7389282895265eb16f5bdc60e50a9442a
Mark Breedlove <mb@dp.la>
2014-09-08 13:21:23 -0400

    Bump version to 32.2.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ out.close()
 from distutils.core import setup
 
 setup( name = 'ingestion',
-       version = '32.1.0',
+       version = '32.2.0',
        description='DPLA Ingestion System',
        author='Digital Public Library of America',
        author_email='tech@dp.la',
