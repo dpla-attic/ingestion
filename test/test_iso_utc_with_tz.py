61eab536a4d88b2f90c8086b73be3acb4ac54d1e
Mark A. Matienzo <mark@dp.la>
2014-09-19 11:02:01 -0400

    Fix UTC datestamp regression (fixes #7699)

--- test/test_iso_utc_with_tz.py
+++ test/test_iso_utc_with_tz.py
@@ -1,12 +1,18 @@
 import sys
 from datetime import datetime
 from dplaingestion.utilities import iso_utc_with_tz
+from time import sleep
 
 def test_iso_utc_with_tz():
     """iso_utc_with_tz() should yield a UTC datetime as a string with timezone symbol Z"""
     utc_now = datetime.utcnow()
     utc_now_iso_string_with_tz = utc_now.isoformat() + "Z"
     assert utc_now_iso_string_with_tz == iso_utc_with_tz(utc_now)
+    # The following was added because of the regression described in #7699.
+    time1 = iso_utc_with_tz()
+    sleep(1)
+    time2 = iso_utc_with_tz() 
+    assert time1 != time2
 
 if __name__ == "__main__":
     raise SystemExit("Use nosetest")
