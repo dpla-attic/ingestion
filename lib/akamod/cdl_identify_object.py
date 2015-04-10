53b3aeab69f1e2f26d42259525b6dbb8d4901a48
Mark A. Matienzo <mark@dp.la>
2015-04-09 22:10:32 -0400

    Update mapping and add cdl_identify_object module

---
+++ lib/akamod/cdl_identify_object.py
@@ -0,0 +1,44 @@
+from akara import logger
+from akara import response
+from akara.services import simple_service
+from amara.thirdparty import json
+from dplaingestion.selector import getprop, setprop, exists
+from akara import module_config
+from amara.lib.iri import is_absolute
+
+@simple_service('POST', 'http://purl.org/la/dp/cdl_identify_object',
+    'cdl_identify_object', 'application/json')
+def cdl_identify_object(body, ctype):
+    """
+    Responsible for: adding a field to a document with the URL where we
+    should expect to the find the thumbnail.
+    """
+
+    try:
+        data = json.loads(body)
+    except:
+        response.code = 500
+        response.add_header('content-type', 'text/plain')
+        return "Unable to parse body as JSON"
+
+    #handle_field = "originalRecord/handle"
+    url = None
+    if exists(data, "object"):
+        handle = getprop(data, "object")
+        for h in (handle if not isinstance(handle, basestring) else [handle]):
+            if is_absolute(h):
+                url = h
+                break
+    if exists(data, "originalRecord/isShownBy"):
+        handle = getprop(data, "originalRecord/isShownBy")
+        for h in (handle if not isinstance(handle, basestring) else [handle]):
+            if is_absolute(h):
+                url = h
+                break
+
+    if url:
+        setprop(data, "object", "url")
+    else:
+        logger.warn("No url found for object in id " % getprop(data, "_id"))
+        delprop(data, "object", True)
+    return json.dumps(data)
