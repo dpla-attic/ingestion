06775fecdd1f5a2bf49f35a128d23a809e2bd2f3
Mark Breedlove <mb@dp.la>
2015-07-20 10:28:06 -0400

    Create release v33.5.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ out.close()
 from distutils.core import setup
 
 setup( name = 'ingestion',
-       version = '33.4.0',
+       version = '33.5.0',
        description='DPLA Ingestion System',
        author='Digital Public Library of America',
        author_email='tech@dp.la',
