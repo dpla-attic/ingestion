addb9a9d199d107033d2ac33ee84e72047d43433
Mark A. Matienzo <mark@dp.la>
2015-12-21 10:15:22 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ out.close()
 from distutils.core import setup
 
 setup( name = 'ingestion',
-       version = '33.7.2',
+       version = '33.7.3',
        description='DPLA Ingestion System',
        author='Digital Public Library of America',
        author_email='tech@dp.la',
