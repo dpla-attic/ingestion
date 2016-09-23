e40d589f9c23f046c160fea79e12c268064fbe62
Scott Williams <scott@dp.la>
2016-09-23 11:18:25 -0400

    Prepare release v33.8.7

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ out.close()
 from distutils.core import setup
 
 setup( name = 'ingestion',
-       version = '33.8.6',
+       version = '33.8.7',
        description='DPLA Ingestion System',
        author='Digital Public Library of America',
        author_email='tech@dp.la',
