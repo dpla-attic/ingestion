230b4f61146689967629fff14618d495ce122ed7
Mark Breedlove <mb@dp.la>
2017-02-01 12:42:31 -0500

    Prepare Release v33.9.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ out.close()
 from distutils.core import setup
 
 setup( name = 'ingestion',
-       version = '33.8.9',
+       version = '33.9.0',
        description='DPLA Ingestion System',
        author='Digital Public Library of America',
        author_email='tech@dp.la',
