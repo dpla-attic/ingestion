823854f914702129a96a4e1dfde9cc0e8ed24e49
Scott Williams <scott@dp.la>
2017-03-03 09:06:30 -0500

    Made provider prefix an optional part of the base id used to create the DPLA identifier. Ensures consistency between IDs generated by Krikri and ingestion1

--- lib/akamod/select-id.py
+++ lib/akamod/select-id.py
@@ -7,24 +7,28 @@ from akara import request, response
 from akara import logger
 from dplaingestion.selector import getprop, setprop, exists
 
-COUCH_ID_BUILDER = lambda src, lname: "--".join((src,lname))
-COUCH_REC_ID_BUILDER = lambda src, id_handle: COUCH_ID_BUILDER(src,id_handle.strip().replace(" ","__"))
+COUCH_ID_BUILDER = lambda src, lname: "--".join((src, lname))
+COUCH_REC_ID_BUILDER = lambda src, id_handle: COUCH_ID_BUILDER(src,
+                                                               CLEAN_ID(
+                                                                   id_handle))
+CLEAN_ID = lambda id_handle: id_handle.strip().replace(" ", "__")
+
 
 @simple_service('POST', 'http://purl.org/la/dp/select-id', 'select-id',
                 'application/json')
-def selid(body, ctype, prop='handle'):
-    '''   
+def selid(body, ctype, prop='handle', use_source='yes'):
+    '''
     Service that accepts a JSON document and adds or sets the "id" property to
     the value of the property named by the "prop" paramater
-    '''   
-    
+    '''
+
     if not prop:
         # Remove this document
         response.code = 500
         response.add_header('content-type', 'text/plain')
         return "No id property has been selected"
 
-    try :
+    try:
         data = json.loads(body)
     except:
         response.code = 500
@@ -35,9 +39,9 @@ def selid(body, ctype, prop='handle'):
     source_name = request_headers.get('Source')
 
     id = None
-    if exists(data,prop):
-        v = getprop(data,prop)
-        if isinstance(v,basestring):
+    if exists(data, prop):
+        v = getprop(data, prop)
+        if isinstance(v, basestring):
             id = v
         else:
             if v:
@@ -52,7 +56,15 @@ def selid(body, ctype, prop='handle'):
         response.add_header('content-type', 'text/plain')
         return "No id property was found"
 
-    data[u'_id'] = COUCH_REC_ID_BUILDER(source_name, id)
-    data[u'id']  = hashlib.md5(data[u'_id']).hexdigest()
+    '''
+    If the useSource parameter is True (default) than prepend it to
+    the id and use that value when hashing for the DPLA id
+    '''
+    if use_source == 'yes':
+        data[u'_id'] = COUCH_REC_ID_BUILDER(source_name, id)
+    else:
+        data[u'_id'] = CLEAN_ID(id)
+
+    data[u'id'] = hashlib.md5(data[u'_id']).hexdigest()
 
     return json.dumps(data)
