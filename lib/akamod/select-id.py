187e69620de3c49ae678812a81292a9763ef6c03
Scott Williams <scott.brandon.williams@gmail.com>
2017-04-05 11:59:01 -0400

    Add pennsylvania (#101)
    * Remove leftover debugging message
    
    * Made provider prefix an optional part of the
    base id used to create the DPLA identifier. Ensures consistency between IDs generated by
    Krikri and ingestion1
    
    * Add PA to create_mapper
    
    * Add PA mapper and profile
    
    * Remove validate_mapv3 enrichment (#100)
    
    We have not been using this, in practice, and do not need the weight
    that it adds to our logfiles.
    
    * Remove validate MAPv3 enrichment
    
    * Remove couch and dpla id minter lambda functions in
    select-id and move them into utilities.py.
    Remove unused imports and code formatting cleanup
    Refactor variable 'id' to not conflict with reserved symbol
    
    * Refactor dcmi_types var name
    
    * Add some select-id tests
    
    * Add new sets to Artstor profile
    
    * Accidentally made changes to the file in build/
    
    * Prepare release v33.10.1
    
    * * Remove method call to map_preview
    * Add description field to collection mapping
    * Mapping changes per https://digitalpubliclibraryofamerica.atlassian.net/browse/DT-1197
       * Fix Date mapping
       * Fix misspelled identifier
       * Fix double mapping of rights
    * Fix collection mapping (no longer maps to isPartOf)
    * Prevent empty collection values from being mapped
    
    * Prepare release v33.10.2
    
    * Add imports for libraries that were not being included
    
    * Add checking to prevent mapping empty values.
    Code cleanup
    
    * Made provider prefix an optional part of the
    base id used to create the DPLA identifier. Ensures consistency between IDs generated by
    Krikri and ingestion1
    
    * Add PA to create_mapper
    
    * Add PA mapper and profile
    
    * Remove validate MAPv3 enrichment
    
    * Remove couch and dpla id minter lambda functions in
    select-id and move them into utilities.py.
    Remove unused imports and code formatting cleanup
    Refactor variable 'id' to not conflict with reserved symbol
    
    * Refactor dcmi_types var name
    
    * Add some select-id tests
    
    * Accidentally made changes to the file in build/
    
    * Add imports for libraries that were not being included
    
    * Add checking to prevent mapping empty values.
    Code cleanup
    
    * added os import back in
    
    * added back imports from develop.

--- lib/akamod/select-id.py
+++ lib/akamod/select-id.py
@@ -4,27 +4,25 @@ from amara.lib.iri import is_absolute
 from akara.services import simple_service
 from akara.util import copy_headers_to_dict
 from akara import request, response
-from akara import logger
-from dplaingestion.selector import getprop, setprop, exists
+from dplaingestion.selector import getprop, exists
+from dplaingestion.utilities import couch_rec_id_builder, clean_id
 
-COUCH_ID_BUILDER = lambda src, lname: "--".join((src,lname))
-COUCH_REC_ID_BUILDER = lambda src, id_handle: COUCH_ID_BUILDER(src,id_handle.strip().replace(" ","__"))
 
 @simple_service('POST', 'http://purl.org/la/dp/select-id', 'select-id',
                 'application/json')
-def selid(body, ctype, prop='handle'):
-    '''   
+def selid(body, ctype, prop='handle', use_source='yes'):
+    '''
     Service that accepts a JSON document and adds or sets the "id" property to
     the value of the property named by the "prop" paramater
-    '''   
-    
+    '''
+
     if not prop:
         # Remove this document
         response.code = 500
         response.add_header('content-type', 'text/plain')
         return "No id property has been selected"
 
-    try :
+    try:
         data = json.loads(body)
     except:
         response.code = 500
@@ -34,25 +32,33 @@ def selid(body, ctype, prop='handle'):
     request_headers = copy_headers_to_dict(request.environ)
     source_name = request_headers.get('Source')
 
-    id = None
-    if exists(data,prop):
-        v = getprop(data,prop)
-        if isinstance(v,basestring):
-            id = v
+    record_id = None
+    if exists(data, prop):
+        v = getprop(data, prop)
+        if isinstance(v, basestring):
+            record_id = v
         else:
             if v:
                 for h in (v if isinstance(v, list) else [v]):
                     if is_absolute(h):
-                        id = h
-                if not id:
-                    id = v[0]
+                        record_id = h
+                if not record_id:
+                    record_id = v[0]
 
-    if not id:
+    if not record_id:
         response.code = 500
         response.add_header('content-type', 'text/plain')
         return "No id property was found"
 
-    data[u'_id'] = COUCH_REC_ID_BUILDER(source_name, id)
-    data[u'id']  = hashlib.md5(data[u'_id']).hexdigest()
+    '''
+    If the useSource parameter is True (default) than prepend it to
+    the id and use that value when hashing for the DPLA id
+    '''
+    if use_source.lower() == 'yes':
+        data[u'_id'] = couch_rec_id_builder(source_name, record_id)
+    else:
+        data[u'_id'] = clean_id(record_id)
+
+    data[u'id'] = hashlib.md5(data[u'_id']).hexdigest()
 
     return json.dumps(data)
